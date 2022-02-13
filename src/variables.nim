b242e17c3f8f500c781c8e83ad323fabcab1e94f
thindil <thindil@laeran.pl>
2022-02-13 05:13:11 +0000

    feat: added command to show help about available subcommands for variables
    FossilOrigin-Name: 8abf146d02459b5a77c2c648537a610a0e6ec44305824ce63a27d3946b0e4a0a

--- src/variables.nim
+++ src/variables.nim
@@ -126,3 +126,15 @@ proc listVariables*(userInput: var OptParser; historyIndex: var int;
     historyIndex = updateHistory("variable list all", db)
     for row in db.fastRows(sql"SELECT id, name, value, description FROM variables"):
       showOutput(row[0] & " " & row[1] & " " & row[2] & " " & row[3])
+
+proc helpVariables*(db: DbConn): int {.gcsafe, sideEffect, locks: 0, raises: [
+    DbError, OSError, IOError, ValueError], tags: [ReadDbEffect, WriteDbEffect,
+    ReadIOEffect, WriteIOEffect].} =
+  ## Show short help about available subcommands related to the environment variables
+  showOutput("""Available subcommands are: list
+
+        To see more information about the subcommand, type help variable [command],
+        for example: help variable list.
+""")
+  return updateHistory("variable", db)
+
