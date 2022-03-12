559ea149233dd6709d749642451122eae4d1830e
thindil <>
2022-03-12 05:00:05 +0000

    feat: better align for the shell's environment variables list
    FossilOrigin-Name: 09595b9bf0a7f08496ae8d841afa9af5d3565d27032c76b56e3772b5853df251

--- src/variables.nim
+++ src/variables.nim
@@ -132,21 +132,24 @@ proc listVariables*(arguments: string; historyIndex: var int;
   let
     nameLength = db.getValue(sql"SELECT name FROM variables ORDER BY LENGTH(name) DESC LIMIT 1").len()
     valueLength = db.getValue(sql"SELECT value FROM variables ORDER BY LENGTH(value) DESC LIMIT 1").len()
+    spacesAmount: Natural = (terminalWidth() / 12).int
   if arguments == "list":
     showFormHeader("Declared environent variables are:")
-    showOutput(message = "ID   $1 $2 Description" % [alignLeft("Name",
-        nameLength), alignLeft("Value", valueLength)], fgColor = fgMagenta)
+    showOutput(message = indent("ID   $1 $2 Description" % [alignLeft("Name",
+        nameLength), alignLeft("Value", valueLength)], spacesAmount),
+            fgColor = fgMagenta)
     for row in db.fastRows(sql(buildQuery(getCurrentDir(),
         "id, name, value, description"))):
-      showOutput(alignLeft(row[0], 4) & " " & alignLeft(row[1], nameLength) &
-          " " & alignLeft(row[2], valueLength) & " " & row[3])
+      showOutput(indent(alignLeft(row[0], 4) & " " & alignLeft(row[1],
+          nameLength) & " " & alignLeft(row[2], valueLength) & " " & row[3], spacesAmount))
   elif arguments == "list all":
     showFormHeader("All declared environent variables are:")
-    showOutput(message = "ID   $1 $2 Description" % [alignLeft("Name",
-        nameLength), alignLeft("Value", valueLength)], fgColor = fgMagenta)
+    showOutput(message = indent("ID   $1 $2 Description" % [alignLeft("Name",
+        nameLength), alignLeft("Value", valueLength)], spacesAmount),
+            fgColor = fgMagenta)
     for row in db.fastRows(sql"SELECT id, name, value, description FROM variables"):
-      showOutput(alignLeft(row[0], 4) & " " & alignLeft(row[1], nameLength) &
-          " " & alignLeft(row[2], valueLength) & " " & row[3])
+      showOutput(indent(alignLeft(row[0], 4) & " " & alignLeft(row[1],
+          nameLength) & " " & alignLeft(row[2], valueLength) & " " & row[3], spacesAmount))
   historyIndex = updateHistory("variable " & arguments, db)
 
 proc helpVariables*(db: DbConn): int {.gcsafe, sideEffect, locks: 0, raises: [
