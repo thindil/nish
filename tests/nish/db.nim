608a6c81d52d5b8bef0f3c2aa220986a2e8e86b4
thindil <>
2022-03-28 04:29:47 +0000

    tests: removed testing alias from db unit tests
    FossilOrigin-Name: f7397fcda1095c228b0e5f6443d2d1418782c6484212bde6d0c96bea0263f8a7

--- tests/nish/db.nim
+++ tests/nish/db.nim
@@ -2,11 +2,9 @@ discard """
   exitcode: 0
 """
 
-import db_sqlite, os
+import os
 import ../../src/nish
 
 let db = startDb("test.db")
 assert fileExists("test.db")
-db.exec(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-    "testalias", "/", 1, "ls -a", "Test alias.")
 quitShell(QuitSuccess, db)
