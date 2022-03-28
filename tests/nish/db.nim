01d0c704a4074bc9d6f26d856796bec2f399d336
thindil <>
2022-03-28 04:10:39 +0000

    tests: added test alias to database test
    FossilOrigin-Name: 54575725f624d8e3ab4cea7674427221635b00f01e3e99f539cb51938cd372df

--- tests/nish/db.nim
+++ tests/nish/db.nim
@@ -2,9 +2,11 @@ discard """
   exitcode: 0
 """
 
-import os
+import db_sqlite, os
 import ../../src/nish
 
 let db = startDb("test.db")
 assert fileExists("test.db")
+db.exec(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+    "testalias", "/", 1, "ls -a", "Test alias.")
 quitShell(QuitSuccess, db)
