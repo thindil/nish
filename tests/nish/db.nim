3544219338ed3e2662403a59ba3654b4e56fff18
thindil <thindil@laeran.pl>
2022-03-30 04:01:22 +0000

    tests: added initialization of shell's commands' history system to database test
    FossilOrigin-Name: ca1671c87ddac5b5c27d7af861b9061a84d4dafe9dd1aaeb56b3c0dd8f5987e7

--- tests/nish/db.nim
+++ tests/nish/db.nim
@@ -2,8 +2,13 @@ discard """
   exitcode: 0
 """
 
-import ../../src/nish
+import std/tables
+import ../../src/[constants, history, nish]
 
 let db = startDb("test.db")
 assert db != nil
+var
+    historyIndex: int
+    helpContent = initTable[string, HelpEntry]()
+historyIndex = initHistory(db, helpContent)
 quitShell(QuitSuccess, db)
