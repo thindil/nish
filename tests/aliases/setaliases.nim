882dd49bf56216e86620ff417f343adffd4cb340
thindil <>
2022-03-30 03:30:19 +0000

    tests: better test for setting the shell's aliases
    FossilOrigin-Name: da4ca5c6245b8a406174f1631c957bbf582dbe9f09f775871c581ff52c0622b2

--- tests/aliases/setaliases.nim
+++ tests/aliases/setaliases.nim
@@ -10,7 +10,11 @@ if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
     if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
         "tests", "/", 1, "ls -a", "Test alias.") == -1:
       quit("Can't add test alias.", QuitFailure)
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
+      quit("Can't add test2 alias.", QuitFailure)
 var myaliases = initOrderedTable[string, int]()
 myaliases.setAliases(getCurrentDir(), db)
+assert parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 2
 assert myaliases.len() == 1
 quitShell(QuitSuccess, db)
