4dccb433d4601c15c80f79046134525cc4c841db
thindil <>
2022-04-02 03:48:19 +0000

    tests: added unit test for initializing aliases
    FossilOrigin-Name: 50df64bc34ae7e0b962e792770dc0602da81b140c848e1b54168e89bb6cd3d71

---
+++ tests/aliases/initaliases.nim
@@ -0,0 +1,22 @@
+discard """
+  exitcode: 0
+"""
+
+import std/[db_sqlite, strutils, tables]
+import ../../src/[aliases, constants, nish]
+
+let db = startDb("test.db")
+if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests", "/", 1, "ls -a", "Test alias.") == -1:
+      quit("Can't add test alias.", QuitFailure)
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
+      quit("Can't add test2 alias.", QuitFailure)
+var
+    myaliases = initOrderedTable[string, int]()
+    helpContent = initTable[string, HelpEntry]()
+myaliases = initAliases(helpContent, db)
+assert myaliases.len() == 1
+assert helpContent.len() == 6
+quitShell(QuitSuccess, db)
