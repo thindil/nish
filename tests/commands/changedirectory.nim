1bf3ba452893d85f342634bae90e9a8efe113d70
thindil <>
2022-04-03 04:03:46 +0000

    tests: added unit test for changing the current directory
    FossilOrigin-Name: 54ad6b35088bd2c66e72536f8bc061e9b891bd29228f1e3ec3076a717c3e2618

---
+++ tests/commands/changedirectory.nim
@@ -0,0 +1,20 @@
+discard """
+  exitcode: 0
+"""
+
+import std/[db_sqlite, os, strutils, tables]
+import ../../src/[aliases, commands, nish]
+
+let db = startDb("test.db")
+if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests", "/", 1, "ls -a", "Test alias.") == -1:
+      quit("Can't add test alias.", QuitFailure)
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
+      quit("Can't add test2 alias.", QuitFailure)
+var
+    myaliases = initOrderedTable[string, int]()
+assert changeDirectory("..", myaliases, db) == QuitSuccess
+assert changeDirectory("/adfwerewtr", myaliases, db) == QuitFailure
+quitShell(QuitSuccess, db)
