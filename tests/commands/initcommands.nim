b27356a168cddfe79da99c67b8b1ad1fddc004de
thindil <thindil@laeran.pl>
2022-04-04 04:09:35 +0000

    tests: added unit test for initCommands
    FossilOrigin-Name: bddc2472091837ad91caef05dd5877b943e996e688eb99caa4da9ab0762fbdab

---
+++ tests/commands/initcommands.nim
@@ -0,0 +1,11 @@
+discard """
+  exitcode: 0
+"""
+
+import std/[tables]
+import ../../src/[commands, constants]
+
+var
+    helpContent = initTable[string, HelpEntry]()
+initCommands(helpContent)
+assert helpContent.len() == 4
