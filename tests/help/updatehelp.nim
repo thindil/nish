9b6a2b40edc4b7269a32f2caa6a05f967265ad77
thindil <>
2022-04-05 04:14:24 +0000

    tests: added unit test for updateHelp
    FossilOrigin-Name: cd43df1318eda709b708cce0b371d26f014b7e4b07e44543ee4d4b5040f478a9

---
+++ tests/help/updatehelp.nim
@@ -0,0 +1,14 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[constants, help, nish]
+
+let db = startDb("test.db")
+assert db != nil
+var
+    helpContent = initTable[string, HelpEntry]()
+updateHelp(helpContent, db)
+assert helpContent.len() == 1
+quitShell(QuitSuccess, db)
