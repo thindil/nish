9a167e34a4b47547c2111b3639fcf6bc4742c53c
thindil <thindil@laeran.pl>
2022-04-06 03:54:43 +0000

    tests: added unit test for showHelp
    FossilOrigin-Name: 628705d63532d89059530fbda5b13ded57fbbc1f41f9163e6955a4827ab0cf1f

---
+++ tests/help/showhelp.nim
@@ -0,0 +1,15 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[constants, help, nish]
+
+let db = startDb("test.db")
+assert db != nil
+var
+    helpContent = initTable[string, HelpEntry]()
+updateHelp(helpContent, db)
+assert showHelp("history show", helpContent, db) == QuitSuccess
+assert showHelp("srewfdsfs", helpContent, db) == QuitFailure
+quitShell(QuitSuccess, db)
