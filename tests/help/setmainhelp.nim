e155fd24cbea9d6d8482b7ec4e90f441c483092d
thindil <>
2022-04-07 03:30:10 +0000

    tests: added unit test for setMainHelp
    FossilOrigin-Name: f871633ca38cfa08c58bb86bbc76c14d8f2c0ff0465e2ed47f24b3be45a610f9

---
+++ tests/help/setmainhelp.nim
@@ -0,0 +1,15 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[constants, help, nish]
+
+let db = startDb("test.db")
+assert db != nil
+var
+    helpContent = initTable[string, HelpEntry]()
+updateHelp(helpContent, db)
+setMainHelp(helpContent)
+assert helpContent.len() == 2
+quitShell(QuitSuccess, db)
