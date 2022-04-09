104b6447b0ced188cb8fe99eb9011a918b33f29b
thindil <thindil@laeran.pl>
2022-04-09 03:27:31 +0000

    tests: added unit test for initHistory
    FossilOrigin-Name: 8448c07959de6271ffa92ba25858bb3ca879200e00f02d411c1da0d3a815963f

---
+++ tests/history/inithistory.nim
@@ -0,0 +1,14 @@
+discard """
+  exitcode: 0
+"""
+
+import std/[tables]
+import ../../src/[constants, history, nish]
+
+let db = startDb("test.db")
+assert db != nil
+var
+    helpContent = initTable[string, HelpEntry]()
+    amount = initHistory(db, helpContent)
+assert amount > -1
+quitShell(QuitSuccess, db)
