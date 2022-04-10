18678677861a17be4502e85793fdb5f3022f1dc5
thindil <thindil@laeran.pl>
2022-04-10 04:44:11 +0000

    tests: added unit test for updateHistory
    FossilOrigin-Name: f6cecedf647ba6a93be707badbe4bd2f0e4793b1e051a315a6f496938f968011

---
+++ tests/history/updatehistory.nim
@@ -0,0 +1,17 @@
+discard """
+  exitcode: 0
+"""
+
+import std/[db_sqlite, tables]
+import ../../src/[constants, history, nish]
+
+let db = startDb("test.db")
+assert db != nil
+var
+    helpContent = initTable[string, HelpEntry]()
+    amount = initHistory(db, helpContent)
+if amount == 0:
+  if db.tryInsertID(sql"INSERT INTO history (command, amount, lastused) VALUES (?, 1, datetime('now'))", "ls -a") == -1:
+    quit("Can't add test command to history.", QuitFailure)
+assert updateHistory("test comm", db) == amount + 1
+quitShell(QuitSuccess, db)
