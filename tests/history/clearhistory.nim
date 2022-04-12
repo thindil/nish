26f4596484c18ee4eeca43cab90e20147e2a1ac8
thindil <>
2022-04-12 04:25:46 +0000

    tests: added unit test for clearHistory
    FossilOrigin-Name: e3ebda2fb10d42f731f88c735087294241bc3ca8635cae3f065ae9c023c6aa37

---
+++ tests/history/clearhistory.nim
@@ -0,0 +1,18 @@
+discard """
+  exitcode: 0
+"""
+
+import std/[db_sqlite, tables]
+import ../../src/[constants, history, nish]
+
+let db = startDb("test.db")
+assert db != nil
+var
+    helpContent = initTable[string, HelpEntry]()
+    amount = initHistory(db, helpContent)
+if amount == 0:
+  if db.tryInsertID(sql"INSERT INTO history (command, amount, lastused) VALUES (?, 1, datetime('now'))", "alias delete") == -1:
+    quit("Can't add test command to history.", QuitFailure)
+assert clearHistory(db) == 0
+assert historyLength(db) == 0
+quitShell(QuitSuccess, db)
