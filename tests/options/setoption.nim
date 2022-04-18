854a6a73371d55273e06b06024eee9d8aee489a9
thindil <>
2022-04-18 03:44:53 +0000

    tests: added unit test for setOption
    FossilOrigin-Name: f9ad72daee98ff9bbadb19240f54cf86238fabece59950f6f0b0d6dc50be4d6d

---
+++ tests/options/setoption.nim
@@ -0,0 +1,16 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[constants, history, nish, options]
+
+let db = startDb("test.db")
+assert db != nil
+var
+    historyIndex: int
+    helpContent = initTable[string, HelpEntry]()
+historyIndex = initHistory(db, helpContent)
+setOption(name = "historyLength", value = "100", db = db)
+assert getOption("historyLength", db) == "100"
+quitShell(QuitSuccess, db)
