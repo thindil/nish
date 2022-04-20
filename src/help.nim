438f1ef071a0fc99c0cd4c1f59cf6971707ff9da
thindil <thindil@laeran.pl>
2022-04-20 03:58:27 +0000

    style: added using named parameters
    FossilOrigin-Name: c2b0f16ea05da10c64384eca61450d47bc2d46f2f327ea95fabc4f4cc2308bc1

--- src/help.nim
+++ src/help.nim
@@ -47,7 +47,7 @@ proc updateHelp*(helpContent; db) {.gcsafe, sideEffect,
   ##
   ## The argument helpContent with updated help for command 'history show'.
   helpContent["history show"] = HelpEntry(usage: "history show",
-      content: "Show the last " & getOption("historyAmount", db) & " commands from the shell's history.")
+      content: "Show the last " & getOption(name = "historyAmount", db = db) & " commands from the shell's history.")
 
 proc showUnknownHelp*(subCommand, command,
     helpType: UserInput): ResultCode {.gcsafe, sideEffect, raises: [], tags: [
@@ -70,9 +70,9 @@ proc showUnknownHelp*(subCommand, command,
               "` for `" & command & "`. To see all available " & helpType &
               " commands, type `" & command & "`.")
 
-proc showHelp*(topic: UserInput; helpContent: HelpTable; db): ResultCode {.gcsafe,
-    sideEffect, raises: [], tags: [ReadIOEffect, WriteIOEffect, ReadDbEffect,
-    WriteDbEffect, ReadEnvEffect, TimeEffect].} =
+proc showHelp*(topic: UserInput; helpContent: HelpTable;
+    db): ResultCode {.gcsafe, sideEffect, raises: [], tags: [ReadIOEffect,
+        WriteIOEffect, ReadDbEffect, WriteDbEffect, ReadEnvEffect, TimeEffect].} =
   ## FUNCTION
   ##
   ## Show the selected help section. If the user entered non-existing name of
