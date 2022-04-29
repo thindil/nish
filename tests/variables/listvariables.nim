0928b7cc596b6ce1b80d235e9a696c1b8038278f
thindil <>
2022-04-29 04:17:29 +0000

    tests: added unit test for listVariables
    FossilOrigin-Name: e3d76a34b62e68915d5e01fa406e423856b06ae8c8aa17eeece9dfdd7214d202

---
+++ tests/variables/listvariables.nim
@@ -0,0 +1,22 @@
+discard """
+  outputsub: Test variable.
+"""
+
+import std/[db_sqlite, strutils, tables]
+import ../../src/[constants, history, nish, variables]
+
+let db = startDb("test.db")
+if parseInt(db.getValue(sql"SELECT COUNT(*) FROM variables")) == 0:
+    if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
+        "TESTS", "/", 1, "test_variable", "Test variable.") == -1:
+      quit("Can't add test variable.", QuitFailure)
+    if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
+        "TESTS2", "/", 0, "test_variable2", "Test variable 2.") == -1:
+      quit("Can't add test2 alias.", QuitFailure)
+var
+    helpContent = initTable[string, HelpEntry]()
+    historyIndex = initHistory(db, helpContent)
+listVariables("list", historyIndex, db)
+listVariables("list all", historyIndex, db)
+listVariables("werwerew", historyIndex, db)
+quitShell(QuitSuccess, db)
