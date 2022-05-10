a9df4348519a4e868367b3d133d546f55be0f684
thindil <thindil@laeran.pl>
2022-05-10 03:44:12 +0000

    feat: updated version number
    FossilOrigin-Name: 583383625c2fe693435594bb0d27dd74344fdd913817c6367e5938fec7df1b4f

--- src/constants.nim
+++ src/constants.nim
@@ -28,7 +28,7 @@ import std/tables
 const
   maxInputLength*: Positive = 4096 # The maximum length of the user input
   aliasNameLength*: Positive = 50  # The maximum length of the shell's alias name
-  shellVersion*: string = "0.2"  # The current version of the project
+  shellVersion*: string = "0.3.0"  # The current version of the project
 
 type
   HelpEntry* = object
