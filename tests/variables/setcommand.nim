f24df5628ea3006e6b59b3ea55f62761f2dad07a
thindil <thindil@laeran.pl>
2022-05-12 04:19:46 +0000

    tests: be sure that database connection was successful
    FossilOrigin-Name: d6613dbf8c24ef50b9028fe1a6f092483b85a143c5ddf2e6546ba25cf831d559

--- tests/variables/setcommand.nim
+++ tests/variables/setcommand.nim
@@ -6,6 +6,7 @@ import std/os
 import ../../src/[nish, variables]
 
 let db = startDb("test.db")
+assert db != nil
 assert setCommand("test=test_val", db) == QuitSuccess
 assert getEnv("test") == "test_val"
 quitShell(QuitSuccess, db)
