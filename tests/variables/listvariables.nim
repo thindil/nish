e5db84cae336e18964cb3dba50aebaccd7981a8e
thindil <thindil@laeran.pl>
2022-05-13 04:45:34 +0000

    tests: moved common code to the separated module
    FossilOrigin-Name: 4c82695e3b1b42f9aae63979aef2219407bb9d5adc3edd0960bc9d1c1f5b8892

--- tests/variables/listvariables.nim
+++ tests/variables/listvariables.nim
@@ -2,18 +2,11 @@ discard """
   outputsub: Test variable.
 """
 
-import std/[db_sqlite, strutils]
 import ../../src/[nish, variables]
 import utils/helpers
 
 var (db, _, historyIndex) = initTest()
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM variables")) == 0:
-    if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
-        "TESTS", "/", 1, "test_variable", "Test variable.") == -1:
-      quit("Can't add test variable.", QuitFailure)
-    if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
-        "TESTS2", "/", 0, "test_variable2", "Test variable 2.") == -1:
-      quit("Can't add test2 alias.", QuitFailure)
+assert setTestVariables(db) == QuitSuccess
 listVariables("list", historyIndex, db)
 listVariables("list all", historyIndex, db)
 listVariables("werwerew", historyIndex, db)
