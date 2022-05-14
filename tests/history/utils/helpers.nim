b5f6e0de70fda0c2aa91fc0ce8a856af9b30b843
thindil <>
2022-05-14 04:19:10 +0000

    tests: added helper module for history tests
    FossilOrigin-Name: f0d208bbea4cac00760fb5f8c311a13e2f9e6e25619a9bc2146536b770d0129a

---
+++ tests/history/utils/helpers.nim
@@ -0,0 +1,9 @@
+import std/[db_sqlite, tables]
+import ../../../src/[constants, history, nish]
+
+proc initTest*(): tuple[db: DbConn, helpContent: HelpTable,
+    historyIndex: HistoryRange] =
+  let db = startDb("test.db")
+  assert db != nil
+  var helpContent = initTable[string, HelpEntry]()
+  return (db, helpContent, initHistory(db, helpContent))
