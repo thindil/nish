258413b4b8551f1f99ed84ccc78569d31ebec8fa
thindil <>
2022-05-15 04:08:50 +0000

    tests: moved common code to the separated module
    FossilOrigin-Name: bcaf481138afd6ea568c474e246b89dd425bf394f3040f46b38880794064bb8b

--- tests/aliases/execalias.nim
+++ tests/aliases/execalias.nim
@@ -2,18 +2,12 @@ discard """
   exitcode: 0
 """
 
-import std/[db_sqlite, os, strutils]
+import std/os
 import ../../src/[aliases, constants, nish]
 import utils/helpers
 
 var (db, _, _, myaliases) = initTest()
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
-    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-        "tests", "/", 1, "ls -a", "Test alias.") == -1:
-      quit("Can't add test alias.", QuitFailure)
-    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
-      quit("Can't add test2 alias.", QuitFailure)
+assert setTestAliases(db) == QuitSuccess
 myaliases.setAliases(getCurrentDir(), db)
 assert execAlias("", "tests", myaliases, db) == QuitSuccess
 assert execAlias("", "tests2", myaliases, db) == QuitFailure
--- tests/aliases/initaliases.nim
+++ tests/aliases/initaliases.nim
@@ -2,18 +2,12 @@ discard """
   exitcode: 0
 """
 
-import std/[db_sqlite, strutils, tables]
+import std/tables
 import ../../src/[aliases, constants, nish]
 import utils/helpers
 
 var (db, helpContent, _, myaliases) = initTest()
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
-    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-        "tests", "/", 1, "ls -a", "Test alias.") == -1:
-      quit("Can't add test alias.", QuitFailure)
-    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
-      quit("Can't add test2 alias.", QuitFailure)
+assert setTestAliases(db) == QuitSuccess
 myaliases = initAliases(helpContent, db)
 assert myaliases.len() == 1
 assert helpContent.len() == 6
