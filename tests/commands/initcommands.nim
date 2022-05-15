84bce1a4210c4d17848aea78df7cbd97e8a9de79
thindil <>
2022-05-15 04:02:29 +0000

    style: code formatting
    FossilOrigin-Name: 797fd91cf993a8ecf0896ffc238af43459885ddd349269af8b35f9206f586f53

--- tests/commands/initcommands.nim
+++ tests/commands/initcommands.nim
@@ -2,10 +2,9 @@ discard """
   exitcode: 0
 """
 
-import std/[tables]
+import std/tables
 import ../../src/[commands, constants]
 
-var
-    helpContent = initTable[string, HelpEntry]()
+var helpContent = initTable[string, HelpEntry]()
 initCommands(helpContent)
 assert helpContent.len() == 4
