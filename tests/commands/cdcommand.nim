dde9cbb5246855903bd3f4bf4c4508c1b6c14d95
thindil <>
2022-05-15 04:03:38 +0000

    tests: moved common code to the separated module
    FossilOrigin-Name: e1e5f0730534431d8ea14831d0fabfea612876d63b1a683e906d54ce8acf84da

--- tests/commands/cdcommand.nim
+++ tests/commands/cdcommand.nim
@@ -2,20 +2,10 @@ discard """
   exitcode: 0
 """
 
-import std/[db_sqlite, strutils, tables]
 import ../../src/[constants, commands, nish]
+import utils/helpers
 
-let db = startDb("test.db")
-assert db != nil
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
-    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-        "tests", "/", 1, "ls -a", "Test alias.") == -1:
-      quit("Can't add test alias.", QuitFailure)
-    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
-      quit("Can't add test2 alias.", QuitFailure)
-var
-    myaliases = initOrderedTable[string, int]()
+var (db, myaliases) = initTest()
 assert cdCommand("/", myaliases, db) == QuitSuccess
 assert cdCommand("/adfwerewtr", myaliases, db) == QuitFailure
 quitShell(ResultCode(QuitSuccess), db)
