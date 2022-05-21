a14c455da29e143ee383e11b5a2ed4546bab64fb
thindil <thindil@laeran.pl>
2022-05-21 05:26:25 +0000

    tests: fixed test for execAlias
    FossilOrigin-Name: 7895412eac5cac85699702f22218bc4a91d6f961edb1903a62ca32684556b1dd

--- tests/aliases/execalias.nim
+++ tests/aliases/execalias.nim
@@ -3,12 +3,12 @@ discard """
 """
 
 import std/os
-import ../../src/[aliases, constants, nish]
+import ../../src/[aliases, constants, lstring, nish]
 import utils/helpers
 
 var (db, _, _, myaliases) = initTest()
 assert setTestAliases(db) == QuitSuccess
 myaliases.setAliases(getCurrentDir(), db)
-assert execAlias("", "tests", myaliases, db) == QuitSuccess
-assert execAlias("", "tests2", myaliases, db) == QuitFailure
+assert execAlias(initLimitedString(capacity = 1), "tests", myaliases, db) == QuitSuccess
+assert execAlias(initLimitedString(capacity = 1), "tests2", myaliases, db) == QuitFailure
 quitShell(ResultCode(QuitSuccess), db)
