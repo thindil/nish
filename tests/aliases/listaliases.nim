ab7389c7378692539dad29db45477b8b95033b80
thindil <>
2022-05-21 05:29:45 +0000

    tests: fixed test for listAliases
    FossilOrigin-Name: 252562ac929712f90a27c226dcfd8fa1951bae02a1531ba51d3c4baf56f0fc83

--- tests/aliases/listaliases.nim
+++ tests/aliases/listaliases.nim
@@ -3,14 +3,14 @@ discard """
 """
 
 import std/[db_sqlite, os, strutils]
-import ../../src/[aliases, constants, nish]
+import ../../src/[aliases, constants, lstring, nish]
 import utils/helpers
 
 var (db, _, historyIndex, myaliases) = initTest()
 assert setTestAliases(db) == QuitSuccess
 myaliases.setAliases(getCurrentDir(), db)
 assert parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 2
-listAliases("list", historyIndex, myaliases, db)
-listAliases("list all", historyIndex, myaliases, db)
-listAliases("werwerew", historyIndex, myaliases, db)
+listAliases(initLimitedString(capacity = 4, text = "list"), historyIndex, myaliases, db)
+listAliases(initLimitedString(capacity = 8, text = "list all"), historyIndex, myaliases, db)
+listAliases(initLimitedString(capacity = 8, text = "werwerew"), historyIndex, myaliases, db)
 quitShell(ResultCode(QuitSuccess), db)
