0351a58da896ff1f2e8a0df8a3d77edef6275026
thindil <thindil@laeran.pl>
2022-05-22 03:58:03 +0000

    tests: fixed test for showHelp
    FossilOrigin-Name: b4f3dadfa737716b347a09f971915f70fd2401a3e0396a0553a09b3dc4bfffcc

--- tests/help/showhelp.nim
+++ tests/help/showhelp.nim
@@ -2,11 +2,11 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[constants, help, nish]
+import ../../src/[constants, help, lstring, nish]
 import utils/helpers
 
 var (db, helpContent) = initTest()
 updateHelp(helpContent, db)
-assert showHelp("history show", helpContent, db) == QuitSuccess
-assert showHelp("srewfdsfs", helpContent, db) == QuitFailure
+assert showHelp(initLimitedString(capacity = 12, text = "history show"), helpContent, db) == QuitSuccess
+assert showHelp(initLimitedString(capacity = 9, text = "srewfdsfs"), helpContent, db) == QuitFailure
 quitShell(ResultCode(QuitSuccess), db)
