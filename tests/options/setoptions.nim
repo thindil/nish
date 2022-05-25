7434cce8bb2f7043c8784478742ecc31f1d2a5ad
thindil <thindil@laeran.pl>
2022-05-25 04:21:27 +0000

    tests: fixed test for setOptions
    FossilOrigin-Name: cfc5e8bc08450db2973adc756adb0cc38d3fd99e8b8548cefdd0defd6e6c7c02

--- tests/options/setoptions.nim
+++ tests/options/setoptions.nim
@@ -2,10 +2,12 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[constants, nish, options]
+import ../../src/[constants, lstring, nish, options]
 import utils/helpers
 
 let db = initTest()
-assert setOptions("set historyLength 1000", db) == QuitSuccess
-assert getOption("historyLength", db) == "1000"
+assert setOptions(initLimitedString(capacity = 22,
+    text = "set historyLength 1000"), db) == QuitSuccess
+assert getOption(initLimitedString(capacity = 13, text = "historyLength"),
+    db) == "1000"
 quitShell(ResultCode(QuitSuccess), db)
