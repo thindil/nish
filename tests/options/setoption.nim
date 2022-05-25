47e97ba27ce4147485052c6997d3c7f18ebdba73
thindil <thindil@laeran.pl>
2022-05-25 04:17:19 +0000

    tests: fixed test for setOption
    FossilOrigin-Name: cf66a6c13bde2905dbdad2b7c6fbca816dab5d4e9344f1f03585ed55c89d2841

--- tests/options/setoption.nim
+++ tests/options/setoption.nim
@@ -2,10 +2,12 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[constants, nish, options]
+import ../../src/[constants, lstring, nish, options]
 import utils/helpers
 
 let db = initTest()
-setOption(optionName = "historyLength", value = "100", db = db)
-assert getOption("historyLength", db) == "100"
+setOption(optionName = initLimitedString(capacity = 13, text = "historyLength"),
+    value = initLimitedString(capacity = 3, text = "100"), db = db)
+assert getOption(initLimitedString(capacity = 13, text = "historyLength"),
+    db) == "100"
 quitShell(ResultCode(QuitSuccess), db)
