3c0449fd7d786b1f8b53911463d2f6e47188d3d3
thindil <thindil@laeran.pl>
2022-05-26 04:14:41 +0000

    tests: fixed test for listVariables
    FossilOrigin-Name: 8e0374cfafe89d98019c5eb2e3d10ebeb319e5bc3991e33f192fc3176f6719bd

--- tests/variables/listvariables.nim
+++ tests/variables/listvariables.nim
@@ -2,12 +2,12 @@ discard """
   outputsub: Test variable.
 """
 
-import ../../src/[constants, nish, variables]
+import ../../src/[constants, lstring, nish, variables]
 import utils/helpers
 
 var (db, _, historyIndex) = initTest()
 assert setTestVariables(db) == QuitSuccess
-listVariables("list", historyIndex, db)
-listVariables("list all", historyIndex, db)
-listVariables("werwerew", historyIndex, db)
+listVariables(initLimitedString(capacity = 4, text = "list"), historyIndex, db)
+listVariables(initLimitedString(capacity = 8, text = "list all"), historyIndex, db)
+listVariables(initLimitedString(capacity = 8, text = "werwerew"), historyIndex, db)
 quitShell(ResultCode(QuitSuccess), db)
