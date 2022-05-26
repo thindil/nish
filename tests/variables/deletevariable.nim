0e9cb9429f8e8777528a7da35b930c10f8f64643
thindil <thindil@laeran.pl>
2022-05-26 03:55:39 +0000

    tests: fixed test for deleteVariable
    FossilOrigin-Name: 609e05dd29f74c89e617637012dca0ffbbe6908b0b48c58db29ef1c3e44c4b19

--- tests/variables/deletevariable.nim
+++ tests/variables/deletevariable.nim
@@ -2,13 +2,17 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[constants, nish, variables]
+import ../../src/[constants, lstring, nish, variables]
 import utils/helpers
 
 var (db, _, historyIndex) = initTest()
 assert setTestVariables(db) == QuitSuccess
-assert deleteVariable("delete 123", historyIndex, db) == QuitFailure
-assert deleteVariable("delete sdf", historyIndex, db) == QuitFailure
-assert deleteVariable("delete 2", historyIndex, db) == QuitSuccess
-assert deleteVariable("delete 2", historyIndex, db) == QuitFailure
+assert deleteVariable(initLimitedString(capacity = 10, text = "delete 123"),
+    historyIndex, db) == QuitFailure
+assert deleteVariable(initLimitedString(capacity = 10, text = "delete sdf"),
+    historyIndex, db) == QuitFailure
+assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"),
+    historyIndex, db) == QuitSuccess
+assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"),
+    historyIndex, db) == QuitFailure
 quitShell(ResultCode(QuitSuccess), db)
