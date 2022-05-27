c4996e2765350fc8ab1d82d9f26d7116f8f3f659
thindil <>
2022-05-27 04:08:10 +0000

    tests: fixed test for unsetCommand
    FossilOrigin-Name: 20e5ac83678b35d50630065a1d05633cb2738c5602500f0245efebf3a7937e42

--- tests/variables/unsetcommand.nim
+++ tests/variables/unsetcommand.nim
@@ -3,12 +3,13 @@ discard """
 """
 
 import std/os
-import ../../src/[constants, nish, variables]
+import ../../src/[constants, lstring, nish, variables]
 
 let db = startDb("test.db")
 assert db != nil
-assert setCommand("test=test_val", db) == QuitSuccess
-assert unsetCommand("test", db) == QuitSuccess
+assert setCommand(initLimitedString(capacity = 13, text = "test=test_val"),
+    db) == QuitSuccess
+assert unsetCommand(initLimitedString(capacity = 4, text = "test"), db) == QuitSuccess
 assert getEnv("test") == ""
-assert unsetCommand("test", db) == QuitSuccess
+assert unsetCommand(initLimitedString(capacity = 4, text = "test"), db) == QuitSuccess
 quitShell(ResultCode(QuitSuccess), db)
