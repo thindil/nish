6e5e7d027f0532110cf0c92289d7664e11e7d039
thindil <thindil@laeran.pl>
2022-05-27 04:05:10 +0000

    tests: fixed test for setCommand
    FossilOrigin-Name: 61ea6703ebbedd776fc0e6003726df40d303a0f92d287b0ca399abba13391674

--- tests/variables/setcommand.nim
+++ tests/variables/setcommand.nim
@@ -3,10 +3,11 @@ discard """
 """
 
 import std/os
-import ../../src/[constants, nish, variables]
+import ../../src/[constants, lstring, nish, variables]
 
 let db = startDb("test.db")
 assert db != nil
-assert setCommand("test=test_val", db) == QuitSuccess
+assert setCommand(initLimitedString(capacity = 13, text = "test=test_val"),
+    db) == QuitSuccess
 assert getEnv("test") == "test_val"
 quitShell(ResultCode(QuitSuccess), db)
