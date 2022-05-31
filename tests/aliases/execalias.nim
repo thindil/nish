1da92d8b72be6a5eb8b43101524fc3bea1dc6fde
thindil <>
2022-05-31 03:44:31 +0000

    tests: fixed test for execAlias
    FossilOrigin-Name: 68bba3d5b6ad068b054862087993b9a6942aac19a312c1a57bdef144c189965c

--- tests/aliases/execalias.nim
+++ tests/aliases/execalias.nim
@@ -9,6 +9,6 @@ import utils/helpers
 var (db, _, _, myaliases) = initTest()
 assert setTestAliases(db) == QuitSuccess
 myaliases.setAliases(getCurrentDir(), db)
-assert execAlias(initLimitedString(capacity = 1), "tests", myaliases, db) == QuitSuccess
-assert execAlias(initLimitedString(capacity = 1), "tests2", myaliases, db) == QuitFailure
+assert execAlias(emptyLimitedString(), "tests", myaliases, db) == QuitSuccess
+assert execAlias(emptyLimitedString(), "tests2", myaliases, db) == QuitFailure
 quitShell(ResultCode(QuitSuccess), db)
