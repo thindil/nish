c1614410562766401c5937c0f62523ca5d2393fd
thindil <>
2022-07-09 03:31:23 +0000

    tests: updated unit test for showHelp procedure
    FossilOrigin-Name: 484d87d58386324b62b98c6116d063b3e8c37e9fc7c4aad0a72d4ccb260fc72e

--- tests/help/showhelp.nim
+++ tests/help/showhelp.nim
@@ -7,6 +7,6 @@ import utils/helpers
 
 var (db, helpContent) = initTest()
 updateHelp(helpContent, db)
-assert showHelp(initLimitedString(capacity = 12, text = "history show"), helpContent, db) == QuitSuccess
+assert showHelp(initLimitedString(capacity = 12, text = "history list"), helpContent, db) == QuitSuccess
 assert showHelp(initLimitedString(capacity = 9, text = "srewfdsfs"), helpContent, db) == QuitFailure
 quitShell(ResultCode(QuitSuccess), db)
