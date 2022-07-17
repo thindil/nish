52a6cee8317f2f79fe0c5b5fb632bbc202c48108
thindil <thindil@laeran.pl>
2022-07-17 04:02:02 +0000

    feat: updated test plugin
    FossilOrigin-Name: e2fa391fc199537e402ae176e9360c4a472fe580202bc98f7fe37d923a282e04

--- tools/testplugin.sh
+++ tools/testplugin.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
 case "${1}" in
-   "") echo "No command specified."; exit 1 ;;
-   install) echo "Installed." ;;
-   uninstall) echo "Uninstalled" ;;
-   enable) echo "Enabled" ;;
-   disable) echo "Disabled" ;;
-   init) echo "Initialized" ;;
-   *) echo "Unknown option"; exit 1 ;;
+   "")
+      echo 'showError "No command specified."'
+      exit 1
+      ;;
+   install)
+      echo 'showOutput "Installed"'
+      ;;
+   uninstall)
+      echo 'showOutput "Uninstalled"'
+      ;;
+   enable)
+      echo 'showOutput "Enabled"'
+      ;;
+   disable)
+      echo 'showOutput "Disabled"'
+      ;;
+   init)
+      echo 'showOutput "Initialized"'
+      ;;
+   *)
+      echo 'showError "Unknown option"'
+      exit 1
+      ;;
 esac
