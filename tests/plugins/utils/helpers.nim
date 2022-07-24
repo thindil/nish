7a3e31c10a495778e690b850ef96cd2dfb5842b9
thindil <>
2022-07-24 03:51:21 +0000

    tests: moved setting the test plugin to separated procedure
    FossilOrigin-Name: 8db659ce8298066dc2707155ee9b37263b720ec5bc004ca536818f6a2eced2e6

--- tests/plugins/utils/helpers.nim
+++ tests/plugins/utils/helpers.nim
@@ -1,5 +1,5 @@
 import std/[db_sqlite, tables]
-import ../../../src/[constants, directorypath, history, nish]
+import ../../../src/[constants, directorypath, lstring, history, nish, plugins, resultcode]
 
 proc initTest*(): tuple[db: DbConn, helpContent: HelpTable,
     historyIndex: HistoryRange] =
@@ -7,3 +7,10 @@ proc initTest*(): tuple[db: DbConn, helpContent: HelpTable,
   assert db != nil
   var helpContent = initTable[string, HelpEntry]()
   return (db, helpContent, initHistory(db, helpContent))
+
+proc setTestPlugin*(db: DbConn; pluginsList: var PluginsList;
+    historyIndex: var HistoryRange): ResultCode =
+  discard removePlugin(db, initLimitedString(capacity = 8, "remove 1"),
+      pluginsList, historyIndex)
+  return addPlugin(db, initLimitedString(capacity = 23,
+      "add tools/testplugin.sh"), pluginsList)
