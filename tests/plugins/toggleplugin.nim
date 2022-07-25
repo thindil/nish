3ef513d7d9c1666fb328a53c99c0f4724e5e90eb
thindil <>
2022-07-25 03:38:29 +0000

    tests: added unit test for togglePlugin procedure
    FossilOrigin-Name: a64a4d06ad3ad547c5b7f5fe110c649140ea3e8bf97e35bb10b28039f255b889

---
+++ tests/plugins/toggleplugin.nim
@@ -0,0 +1,18 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[lstring, nish, plugins, resultcode]
+import utils/helpers
+
+var (db, _, historyIndex) = initTest()
+var pluginsList: PluginsList = initTable[string, string]()
+assert setTestPlugin(db, pluginsList, historyIndex) == QuitSuccess
+assert togglePlugin(db, initLimitedString(capacity = 9, "disable 1"),
+    pluginsList, historyIndex) == QuitSuccess
+assert togglePlugin(db, initLimitedString(capacity = 8, "enable 1"),
+    pluginsList, historyIndex, false) == QuitSuccess
+assert togglePlugin(db, initLimitedString(capacity = 8, "enable 2"),
+    pluginsList, historyIndex, false) == QuitFailure
+quitShell(QuitSuccess.ResultCode, db)
