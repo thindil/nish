67dd17a632005893cca88aec25ca5bc4c887234d
thindil <thindil@laeran.pl>
2022-07-26 03:41:14 +0000

    tests: added unit test for listPlugins procedure
    FossilOrigin-Name: cb60e72fb9bfb89ffb7e1bb6383099f1adaaad808e6c6433975f477a1c79628c

---
+++ tests/plugins/listplugins.nim
@@ -0,0 +1,18 @@
+discard """
+  outputsub: plugins are
+"""
+
+import std/tables
+import ../../src/[lstring, nish, plugins, resultcode]
+import utils/helpers
+
+var (db, _, historyIndex) = initTest()
+var pluginsList: PluginsList = initTable[string, string]()
+assert setTestPlugin(db, pluginsList, historyIndex) == QuitSuccess
+listPlugins(initLimitedString(capacity = 4, text = "list"), historyIndex,
+    pluginsList, db)
+listPlugins(initLimitedString(capacity = 8, text = "list all"), historyIndex,
+    pluginsList, db)
+listPlugins(initLimitedString(capacity = 8, text = "werwerew"), historyIndex,
+    pluginsList, db)
+quitShell(ResultCode(QuitSuccess), db)
