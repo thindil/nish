f44d61b1b8e3304ec56b85d0648a6ada0fe0879c
thindil <thindil@laeran.pl>
2022-07-26 03:42:09 +0000

    tests: updated unit test for execPlugin
    FossilOrigin-Name: 8ad966b3aefbd7663e00db199815b0070c9f67198dcdb11df6691f477288bb21

--- tests/plugins/execplugin.nim
+++ tests/plugins/execplugin.nim
@@ -2,9 +2,10 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[nish, plugins, resultcode]
+import ../../src/[nish, lstring, plugins, resultcode]
 import utils/helpers
 
 let (db, _, _) = initTest()
-assert execPlugin("tools/testplugin.sh", ["init"], db) == QuitSuccess
+assert execPlugin("tools/testplugin.sh", ["init"], db).code == QuitSuccess
+assert execPlugin("tools/testplugin.sh", ["info"], db).answer.len() > 0
 quitShell(QuitSuccess.ResultCode, db)
