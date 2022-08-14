5c8bc99bf408427db2c4a03ea7ea192598d08a5e
thindil <>
2022-08-14 03:38:47 +0000

    tests: added contracts for setCommand procedure
    FossilOrigin-Name: 75dbb73a3d27e84ea81ec156d6c523edb0ee3c4b7a14138c288c7926ea09f2a2

--- src/variables.nim
+++ src/variables.nim
@@ -190,7 +190,7 @@ proc initVariables*(helpContent: var HelpTable; db) {.gcsafe, sideEffect,
 
 proc setCommand*(arguments; db): ResultCode {.gcsafe, sideEffect, raises: [],
     tags: [ReadIOEffect, ReadDbEffect, WriteIOEffect, WriteDbEffect,
-        ReadEnvEffect, TimeEffect].} =
+    ReadEnvEffect, TimeEffect], contractual.} =
   ## FUNCTION
   ##
   ## Build-in command to set the selected environment variable
@@ -204,23 +204,26 @@ proc setCommand*(arguments; db): ResultCode {.gcsafe, sideEffect, raises: [],
   ##
   ## QuitSuccess if the environment variable was successfully set, otherwise
   ## QuitFailure
-  if arguments.len() > 0:
-    let varValues: seq[string] = split(s = $arguments, sep = '=')
-    if varValues.len() > 1:
-      try:
-        putEnv(key = varValues[0], val = varValues[1])
-        showOutput(message = "Environment variable '" & varValues[0] &
-            "' set to '" & varValues[1] & "'", fgColor = fgGreen)
-        result = QuitSuccess.ResultCode
-      except OSError:
-        result = showError(message = "Can't set the environment variable '" &
-            varValues[0] & "'. Reason:", e = getCurrentException())
+  require:
+    db != nil
+  body:
+    if arguments.len() > 0:
+      let varValues: seq[string] = split(s = $arguments, sep = '=')
+      if varValues.len() > 1:
+        try:
+          putEnv(key = varValues[0], val = varValues[1])
+          showOutput(message = "Environment variable '" & varValues[0] &
+              "' set to '" & varValues[1] & "'", fgColor = fgGreen)
+          result = QuitSuccess.ResultCode
+        except OSError:
+          result = showError(message = "Can't set the environment variable '" &
+              varValues[0] & "'. Reason:", e = getCurrentException())
+      else:
+        result = showError(message = "You have to enter the name of the variable and its value.")
     else:
       result = showError(message = "You have to enter the name of the variable and its value.")
-  else:
-    result = showError(message = "You have to enter the name of the variable and its value.")
-  discard updateHistory(commandToAdd = "set " & arguments, db = db,
-      returnCode = result)
+    discard updateHistory(commandToAdd = "set " & arguments, db = db,
+        returnCode = result)
 
 proc unsetCommand*(arguments; db): ResultCode {.gcsafe, sideEffect, raises: [],
     tags: [ReadIOEffect, ReadDbEffect, WriteIOEffect, WriteDbEffect,
