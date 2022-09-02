8151e91a7be7fc5cfd503b1328126be89398ea38
thindil <>
2022-09-02 05:00:51 +0000

    tests: added unit test for addCommand procedure
    FossilOrigin-Name: 407ead989396a40907782a8d7e1f2f6b9bf104b56d07d4c7628a715622cdf0a3

---
+++ tests/commands/addcommand.nim
@@ -0,0 +1,28 @@
+discard """
+  exitcode: 0
+"""
+
+import std/[db_sqlite, tables]
+import contracts
+import ../../src/[commands, constants, lstring, nish, resultcode]
+import utils/helpers
+
+var
+  (db, _) = initTest()
+  commandsList: CommandsList = initTable[string, CommandProc]()
+
+proc testCommand(arguments: UserInput; db: DbConn): ResultCode {.gcsafe,
+      raises: [], contractual.} =
+  body:
+    echo "test"
+
+addCommand(name = initLimitedString(capacity = 4, text = "test"),
+    command = testCommand, commands = commandsList)
+assert commandsList.len() == 1
+addCommand(name = initLimitedString(capacity = 4, text = "test"),
+    command = testCommand, commands = commandsList)
+assert commandsList.len() == 1
+addCommand(name = initLimitedString(capacity = 4, text = "exit"),
+    command = testCommand, commands = commandsList)
+assert commandsList.len() == 1
+quitShell(ResultCode(QuitSuccess), db)
