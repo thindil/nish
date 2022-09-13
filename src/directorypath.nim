8d20efc94f5252d82e9aa220a082615126054b5c
thindil <>
2022-09-13 04:00:23 +0000

    docs: added documentation about imports
    FossilOrigin-Name: c4ed375dc7bf63a39c3d6858646cafd448fa9898fce53d2b2ed1bf48909ff889

--- src/directorypath.nim
+++ src/directorypath.nim
@@ -23,6 +23,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# Standard library imports
 import std/strutils
 
 type DirectoryPath* = distinct string
