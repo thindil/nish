6538dd86a69de1b7c70c2dc5ad2d7a0bb2d99b54
thindil <>
2022-09-13 04:00:16 +0000

    docs: added documentation about imports
    FossilOrigin-Name: 10d32d49d208124d69c252fdca33bdf49c61be5fdfe5e392af7bf495bc999f32

--- src/constants.nim
+++ src/constants.nim
@@ -23,7 +23,9 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# Standard library imports
 import std/tables
+# Internal imports
 import lstring
 
 const
