68ac80a03bb255953084dde6a1e9a2547d6fefd0
thindil <>
2022-09-16 03:35:02 +0000

    docs: added documentation about imports
    FossilOrigin-Name: 409633ac0f0f3eaf3de8645d3654f8afd553fffc58e52007d6bac45a2fb06fd1

--- src/nish.nim
+++ src/nish.nim
@@ -23,8 +23,11 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# Standard library imports
 import std/[db_sqlite, os, osproc, parseopt, strutils, tables, terminal]
+# External modules imports
 import contracts
+# Internal imports
 import aliases, commands, commandslist, completion, constants, directorypath,
     help, history, input, lstring, options, output, plugins, prompt, resultcode, variables
 
