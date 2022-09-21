1228e1d88bac0a3478b5fe41ae380e364dd38df0
thindil <>
2022-09-21 04:30:28 +0000

    tests: moved setting pluginsList and commands to initTest
    FossilOrigin-Name: 037476c8b663950b247b4227bc3ed08d8fd22a418f85549d13afa50090ee5f2f

--- tests/plugins/utils/helpers.nim
+++ tests/plugins/utils/helpers.nim
@@ -2,11 +2,14 @@ import std/[db_sqlite, tables]
 import ../../../src/[commandslist, constants, directorypath, lstring, nish,
     plugins, resultcode]
 
-proc initTest*(): tuple[db: DbConn, helpContent: ref HelpTable] =
+proc initTest*(): tuple[db: DbConn, helpContent: ref HelpTable, pluginsList: ref PluginsList, commands: ref CommandsList] =
   let db = startDb("test.db".DirectoryPath)
   assert db != nil
-  var helpContent = newTable[string, HelpEntry]()
-  return (db, helpContent)
+  var
+    helpContent = newTable[string, HelpEntry]()
+    pluginsList = newTable[string, PluginData]()
+    commands = newTable[string, CommandData]()
+  return (db, helpContent, pluginsList, commands)
 
 proc setTestPlugin*(db: DbConn; pluginsList: ref PluginsList;
     commands: ref CommandsList): ResultCode =
