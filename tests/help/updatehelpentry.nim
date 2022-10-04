d856a2190ce6db1027fc436c24fd21e34ad10948
thindil <thindil@laeran.pl>
2022-10-04 03:43:43 +0000

    tests: updated unit tests
    FossilOrigin-Name: 1bfa22fccb7d5ee2ae60ddb4dfefea6325041b30ce59eba8e1853d30a57442fe

--- tests/help/updatehelpentry.nim
+++ tests/help/updatehelpentry.nim
@@ -6,6 +6,7 @@ import ../../src/[help, lstring, resultcode]
 import utils/helpers
 
 var (db, _) = initTest()
+discard deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db)
 assert addHelpEntry(initLimitedString(capacity = 4, text = "test"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
     capacity = 4, text = "test"), "test help", db) == QuitSuccess
