665793463d9b307eb430ef75e1f39d607967bd91
thindil <thindil@laeran.pl>
2022-10-06 04:10:23 +0000

    tests: updated test for updateHelpEntry
    FossilOrigin-Name: f6684e4e02372e63885b721cef03f6a3874753e2c502227c952a97d758678dcd

--- tests/help/updatehelpentry.nim
+++ tests/help/updatehelpentry.nim
@@ -12,7 +12,7 @@ assert addHelpEntry(initLimitedString(capacity = 4, text = "test"),
     capacity = 4, text = "test"), "test help", false, db) == QuitSuccess
 assert updateHelpEntry(initLimitedString(capacity = 4, text = "test"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help2", db) == QuitSuccess
+    capacity = 4, text = "test"), "test help2", db, false) == QuitSuccess
 assert updateHelpEntry(initLimitedString(capacity = 4, text = "asdd"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help2", db) == QuitFailure
+    capacity = 4, text = "test"), "test help2", db, false) == QuitFailure
