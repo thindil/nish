377d870d962b203609cf5299f65f54ca13f5c961
thindil <thindil@laeran.pl>
2022-10-14 04:29:35 +0000

    feat: added API call for updating help entries
    FossilOrigin-Name: bb762fddf31a3ad9201dc9672c14671644ce6e1a915af421634ffcf1ff96fb3a

--- src/plugins.nim
+++ src/plugins.nim
@@ -245,6 +245,20 @@ proc execPlugin*(pluginPath: string; arguments: openArray[string]; db;
                 capacity = maxNameLength, text = remainingOptions[0]),
                 db = db) == QuitFailure:
               break
+          # Update the help entry in the shell. The argument is the topic of the help,
+          # its usage and the content
+          of "updateHelp":
+            let remainingOptions = options.remainingArgs()
+            if remainingOptions.len() < 3:
+              showError(message = "Insufficient arguments for updateHelp.")
+              break
+            if  updateHelpEntry(topic = initLimitedString(capacity = maxNameLength,
+                text = remainingOptions[0]), usage = initLimitedString(
+                capacity = maxInputLength, text = remainingOptions[1]),
+                plugin = initLimitedString(capacity = maxInputLength,
+                text = pluginPath), content = remainingOptions[2],
+                isTemplate = false, db = db) == QuitFailure:
+              break
           # The plugin sent any unknown request or response, show error about it
           else:
             showError(message = "Unknown request or response from the plugin '" &
