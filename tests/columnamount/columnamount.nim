425acefae0be244e1a4d44a3cd6aafad23f9ec6a
thindil <thindil@laeran.pl>
2022-10-21 03:50:33 +0000

    tests: merge columnamount tests into one file
    FossilOrigin-Name: 2b21057ba090b4304106cdf8a8b9cf8ffdd43a0a0febf1089764c7d3401c2216

--- tests/columnamount/columnamount.nim
+++ tests/columnamount/columnamount.nim
@@ -4,6 +4,9 @@ discard """
 
 import ../../src/columnamount
 
+let code: ColumnAmount = 42.ColumnAmount
+assert code == 42
+
 assert (12.ColumnAmount / 2).int == 6
 assert (10.ColumnAmount - 5) == 5
 assert (5.ColumnAmount * 2) == 10
