d900762d013d277e65d06e1bb111e12337b55360
thindil <thindil@laeran.pl>
2022-10-26 04:23:22 +0000

    tests: added test for take character from LimitedString
    FossilOrigin-Name: 3f5361078fde4110c5b7e193ed1e1d0c62682bfb43ed42063e9219f59b35304d

--- tests/lstring/lstring.nim
+++ tests/lstring/lstring.nim
@@ -98,6 +98,10 @@ block:
 
 block:
   let testString: LimitedString = initLimitedString(capacity = 4, text = "test")
+  assert testString[1] == 'e'
+
+block:
+  let testString: LimitedString = initLimitedString(capacity = 4, text = "test")
   assert testString.startsWith("te")
   assert not testString.startsWith("as")
 
