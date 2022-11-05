15b868b4a9d8efa8b88ef55ced2c0ad7c592c436
thindil <thindil@laeran.pl>
2022-11-05 05:26:00 +0000

    tests: added unit test for getFormattedDir procedure
    FossilOrigin-Name: 5f0a9ba851d054361fd78d7937699abcea87560c14b747053dc3acd6e72fc8a4

--- tests/prompt/prompt.nim
+++ tests/prompt/prompt.nim
@@ -6,4 +6,6 @@ import ../../src/[nish, directorypath, prompt, resultcode]
 
 let db = startDb("test.db".DirectoryPath)
 assert db != nil
+
+assert getFormattedDir().len() > 0
 showPrompt(true, "ls -a", ResultCode(QuitSuccess), db)
