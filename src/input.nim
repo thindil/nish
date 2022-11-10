e75c84b58d393607b04e18fc91f980e57c78e557
thindil <thindil@laeran.pl>
2022-11-10 05:11:32 +0000

    feat: added moving cursor in various forms
    FossilOrigin-Name: 8349bff5bb931008e0267ce53b0b7c68a32f1d79ec8a1d556708e465cdf10148

--- src/input.nim
+++ src/input.nim
@@ -69,6 +69,7 @@ proc readInput*(maxLength: MaxInputLength = maxInputLength): UserInput {.gcsafe,
     var
       inputChar: char = '\0'
       resultString: LimitedString = emptyLimitedString(capacity = maxLength)
+      cursorPosition: Natural = 0
     # Read the user input until not meet new line character or the input
     # reach the maximum length
     while inputChar.ord() != 13 and resultString.len() < maxLength:
@@ -95,6 +96,36 @@ proc readInput*(maxLength: MaxInputLength = maxInputLength): UserInput {.gcsafe,
         # Escape key pressed, return "exit" as input value
         if inputChar.ord() == 27:
           return exitString
+        elif inputChar in ['[', 'O']:
+          try:
+            inputChar = getch()
+          except IOError:
+            showError(message = "Can't get the next character after Escape. Reason: ",
+                e = getCurrentException())
+            return exitString
+          try:
+            # Arrow left key pressed
+            if inputChar == 'D' and resultString.len() > 0 and
+                cursorPosition > 0:
+              stdout.cursorBackward()
+              cursorPosition.dec()
+            # Arrow right key pressed
+            elif inputChar == 'C' and resultString.len() > 0 and
+                cursorPosition < runeLen(s = $resultString):
+              stdout.cursorForward()
+              cursorPosition.inc()
+            # Home key pressed
+            elif inputChar == 'H' and cursorPosition > 0:
+              stdout.cursorBackward(count = cursorPosition)
+              cursorPosition = 0
+            # End key pressed
+            elif inputChar == 'F' and cursorPosition < runeLen(
+                s = $resultString):
+              stdout.cursorForward(count = runeLen(s = $resultString) - cursorPosition)
+              cursorPosition = runeLen(s = $resultString)
+          except IOError, ValueError:
+            showError(message = "Can't move the cursor. Reason: ",
+                e = getCurrentException())
         else:
           continue
       # Visible character, add it to the user input string and show it in the
@@ -119,6 +150,7 @@ proc readInput*(maxLength: MaxInputLength = maxInputLength): UserInput {.gcsafe,
               e = getCurrentException())
         try:
           resultString.add(y = inputRune)
+          cursorPosition.inc()
         except CapacityError:
           return resultString
       try:
