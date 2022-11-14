c161725f1df93cee46dbd658fb814fbb19202f76
thindil <thindil@laeran.pl>
2022-11-14 04:29:11 +0000

    tests: added messages to asserts
    FossilOrigin-Name: 97c46e91c5a74e962d94c0591b765b8c311e3e4c303046f334c10a57c3ff34a6

--- tests/commandslist/commandslist.nim
+++ tests/commandslist/commandslist.nim
@@ -7,7 +7,7 @@ import ../../src/[commandslist, constants, directorypath, lstring, nish, resultc
 import contracts
 
 let db = startDb("test.db".DirectoryPath)
-assert db != nil
+assert db != nil, "Failed to initialized database."
 if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
   if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
       "tests", "/", 1, "ls -a", "Test alias.") == -1:
@@ -26,21 +26,21 @@ proc testCommand*(arguments: UserInput; db: DbConn;
 # Add a command
 addCommand(name = initLimitedString(capacity = 4, text = "test"),
     command = testCommand, commands = commands)
-assert commands.len() == 1
+assert commands.len() == 1, "Failed to add a new command."
 # Try to add again the same command
 try:
   addCommand(name = initLimitedString(capacity = 4, text = "test"),
       command = testCommand, commands = commands)
 except CommandsListError:
   discard
-assert commands.len() == 1
+assert commands.len() == 1, "Failed to not add an existing command."
 # Try to replace built-in command
 try:
   addCommand(name = initLimitedString(capacity = 4, text = "exit"),
       command = testCommand, commands = commands)
 except CommandsListError:
   discard
-assert commands.len() == 1
+assert commands.len() == 1, "Failed to not replace a built-in command."
 
 proc testCommand2(arguments: UserInput; db: DbConn;
     list: CommandLists): ResultCode {.gcsafe, raises: [], contractual.} =
@@ -60,16 +60,16 @@ except CommandsListError:
 # Delete the command
 deleteCommand(name = initLimitedString(capacity = 4, text = "test"),
     commands = commands)
-assert commands.len() == 0
+assert commands.len() == 0, "Failed to delete a command."
 # Try to delete non-existing command
 addCommand(name = initLimitedString(capacity = 5, text = "test2"),
     command = testCommand, commands = commands)
-assert commands.len() == 1
+assert commands.len() == 1, "Failed to not delete a non-existing command"
 try:
   deleteCommand(name = initLimitedString(capacity = 4, text = "test"),
       commands = commands)
 except CommandsListError:
   discard
-assert commands.len() == 1
+assert commands.len() == 1, "Failed to not delete a previously delete command"
 
 quitShell(ResultCode(QuitSuccess), db)
