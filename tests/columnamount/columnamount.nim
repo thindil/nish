223ea4d88d2d25aa25be7a58113b1e34b2782880
thindil <thindil@laeran.pl>
2022-11-14 04:28:52 +0000

    tests: added messages to asserts
    FossilOrigin-Name: 1e0fae2240878ea09f03890e0fef9f4886633497a0fe0ba33255c8643fc58acf

--- tests/columnamount/columnamount.nim
+++ tests/columnamount/columnamount.nim
@@ -5,8 +5,8 @@ discard """
 import ../../src/columnamount
 
 let code: ColumnAmount = 42.ColumnAmount
-assert code == 42
+assert code == 42, "Failed to compare ColumnAmount."
 
-assert (12.ColumnAmount / 2).int == 6
-assert (10.ColumnAmount - 5) == 5
-assert (5.ColumnAmount * 2) == 10
+assert (12.ColumnAmount / 2).int == 6, "Failed to divide ColumnAmount"
+assert (10.ColumnAmount - 5) == 5, "Failed to substract ColumnAmount"
+assert (5.ColumnAmount * 2) == 10, "Failed to multiply ColumnAmount"
