4aa5edcd3ca3adfcd0b02d6c076f2db25117b483
thindil <thindil@laeran.pl>
2022-11-14 04:28:44 +0000

    tests: added messages to asserts
    FossilOrigin-Name: 71292b54f45af4ce2d6738da26705eb7c0d204728a5a2e7d72c4791d8cc31a43

--- tests/aliases/aliases.nim
+++ tests/aliases/aliases.nim
@@ -7,7 +7,7 @@ import ../../src/[aliases, directorypath, commandslist, lstring, nish, resultcod
 import contracts
 
 let db = startDb("test.db".DirectoryPath)
-assert db != nil
+assert db != nil, "No connection to database."
 var
   myaliases = newOrderedTable[LimitedString, int]()
   commands = newTable[string, CommandData]()
@@ -22,26 +22,28 @@ if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 1:
     quit("Can't add the second test alias.")
 
 initAliases(db, myaliases, commands)
-assert myaliases.len() == 1
+assert myaliases.len() == 1, "Failed to set aliases for current directory."
 
 
-assert deleteAlias(initLimitedString(capacity = 8, text = "delete 2"), myaliases, db) == QuitSuccess
+assert deleteAlias(initLimitedString(capacity = 8, text = "delete 2"),
+    myaliases, db) == QuitSuccess, "Failed to delete an alias."
 assert parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 1
-assert deleteAlias(initLimitedString(capacity = 9, text = "delete 22"), myaliases, db) == QuitFailure
+assert deleteAlias(initLimitedString(capacity = 9, text = "delete 22"),
+    myaliases, db) == QuitFailure, "Failed to not delete a non-existing alias."
 if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
     "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
   quit("Can't add test2 alias.")
-assert parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 2
+assert parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 2, "Failed to re-add an alias."
 
 myaliases.setAliases(getCurrentDir().DirectoryPath, db)
-assert execAlias(emptyLimitedString(), "tests", myaliases, db) == QuitSuccess
-assert execAlias(emptyLimitedString(), "tests2", myaliases, db) == QuitFailure
+assert execAlias(emptyLimitedString(), "tests", myaliases, db) == QuitSuccess, "Failed to execute an alias."
+assert execAlias(emptyLimitedString(), "tests2", myaliases, db) == QuitFailure, "Failed to not execute a noon-existing alias."
 
 assert parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 2
 assert listAliases(initLimitedString(capacity = 4, text = "list"), myaliases,
-    db) == QuitSuccess
+    db) == QuitSuccess, "Failed to show the list of available aliases."
 assert listAliases(initLimitedString(capacity = 8, text = "list all"),
-    myaliases, db) == QuitSuccess
+    myaliases, db) == QuitSuccess, "Failed to show the list of all aliases."
 try:
   assert listAliases(initLimitedString(capacity = 8, text = "werwerew"),
       myaliases, db) == QuitSuccess
