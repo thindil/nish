12563231089bccd886f7bee72a22666869b0d7a1
thindil <thindil@laeran.pl>
2022-11-15 05:13:01 +0000

    tests: added messages to asserts
    FossilOrigin-Name: ccd9f1403bfb87b7a74256b5d050f9503f6b320cad9851fa215b51f09e44eef9

--- tests/input/input.nim
+++ tests/input/input.nim
@@ -11,31 +11,32 @@ var
   arguments: UserInput = getArguments(userCommand, conjCommands)
 
 assert arguments == initLimitedString(capacity = maxInputLength,
-    text = "ls -a -b --foo --bar=20 file.txt")
+    text = "ls -a -b --foo --bar=20 file.txt"), "Failed to set the argumets."
 
 assert readInput() == initLimitedString(capacity = maxInputLength, text = "exit")
 
-assert readChar('c') == "c"
-assert readChar('H') == "H"
+assert readChar('c') == "c", "Failed to read a character from the input."
+assert readChar('H') == "H", "Failed to read a upper character from the input."
 
 block:
   var
     inputString = initLimitedString(capacity = maxInputLength, text = "my text")
     cursorPosition: Natural = 1
   deleteChar(inputString, cursorPosition)
-  assert inputString == "y text"
-  assert cursorPosition == 0
+  assert inputString == "y text", "Failed to delete character from the input."
+  assert cursorPosition == 0, "Failed to get cursor position after deleting a character."
 
 block:
-  let inputString = initLimitedString(capacity = maxInputLength, text = "my text")
+  let inputString = initLimitedString(capacity = maxInputLength,
+      text = "my text")
   var cursorPosition: Natural = 1
   moveCursor('D', cursorPosition, inputString)
-  assert cursorPosition == 0
+  assert cursorPosition == 0, "Failed to move the cursor back in the input."
 
 block:
   var
     inputString = initLimitedString(capacity = maxInputLength, text = "my text")
     cursorPosition: Natural = 7
   updateInput(cursorPosition, inputString, false, "a")
-  assert inputString == "my texta"
-  assert cursorPosition == 8
+  assert inputString == "my texta", "Failed to insert a character at the end of the input."
+  assert cursorPosition == 8, "Failed to get the cursor position after inserting a character."
