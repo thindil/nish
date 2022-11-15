e7cb74bef8f08b51c56dffc55ee9c8d176b58363
thindil <thindil@laeran.pl>
2022-11-15 05:13:07 +0000

    tests: added messages to asserts
    FossilOrigin-Name: 29d31f1c5bff2a9f3ffae6f8bf1feafb69d0851fa78139ec5ec0a811b318cf92

--- tests/lstring/lstring.nim
+++ tests/lstring/lstring.nim
@@ -7,48 +7,49 @@ import ../../src/[input, lstring]
 block:
   var testString: LimitedString = initLimitedString(capacity = 14, text = "test")
   testString.add(" and test")
-  assert $testString == "test and test"
+  assert $testString == "test and test", "Failed to append a string to LimitedString."
   testString.add("2")
-  assert $testString == "test and test2"
+  assert $testString == "test and test2", "Failed to append another string to LimitedString."
   try:
     testString.add("very long text outside of max allowed lenght")
   except CapacityError:
-    assert $testString == "test and test2"
+    assert $testString == "test and test2", "Failed to not append too long string to LimitedString"
 
 block:
   let testString: LimitedString = initLimitedString(capacity = 14, text = "test")
-  assert testString.len() == 4
-  assert testString.capacity == 14
+  assert testString.len() == 4, "Failed to get the length of LimitedString."
+  assert testString.capacity == 14, "Failed to get the capacity of LimitedString."
 
 block:
   let testString: LimitedString = initLimitedString(capacity = 4, text = "test")
-  assert testString != "test2"
-  assert testString == "test"
+  assert testString != "test2", "Failed to compare different LimitedString and string."
+  assert testString == "test", "Failed to compare the same LimitedString and string."
 
 block:
   let testString: LimitedString = initLimitedString(capacity = 4, text = "test")
-  assert testString & "test" == "testtest"
-  assert "new" & testString == "newtest"
+  assert testString & "test" == "testtest", "Failed to append a string to LimitedString."
+  assert "new" & testString == "newtest", "Failed to preprend a string to LimitedString."
 
 block:
   let testString: LimitedString = initLimitedString(capacity = 4, text = "test")
-  assert $testString == "test"
+  assert $testString == "test", "Failed to convert LimitedString to string."
 
 block:
   let testString: LimitedString = emptyLimitedString(maxInputLength)
-  assert testString.capacity == maxInputLength
-  assert testString == ""
+  assert testString.capacity == maxInputLength, "Failed to set the proper capacity for an empty LimitedString."
+  assert testString == "", "Failed to set the proper text for an empty LimitedString."
 
 block:
   let testString: LimitedString = initLimitedString(capacity = 4, text = "test")
-  assert testString.find('e') == 1
-  assert testString.find('a') == -1
+  assert testString.find('e') == 1, "Failed to find a character in LimitedString"
+  assert testString.find('a') == -1, "Failed to not find a character in LimitedString"
 
 block:
   let testString: LimitedString = initLimitedString(capacity = 4, text = "test")
   assert $testString == "test"
   try:
-    let testString2: LimitedString = initLimitedString(capacity = 4, text = "too long text")
+    let testString2: LimitedString = initLimitedString(capacity = 4,
+        text = "too long text")
     assert $testString2 == "too long text"
   except CapacityError:
     quit 0
@@ -93,7 +94,8 @@ block:
   assert not testString.startsWith("as")
 
 block:
-  var testString: LimitedString = initLimitedString(capacity = 10, text = "old text")
+  var testString: LimitedString = initLimitedString(capacity = 10,
+      text = "old text")
   testString.text = "new text"
   assert testString == "new text"
   try:
