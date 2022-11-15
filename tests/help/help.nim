4b7ad13f1298bbf56fbf2d965ae34ef1e872dc6d
thindil <thindil@laeran.pl>
2022-11-15 05:12:45 +0000

    tests: added messages to asserts
    FossilOrigin-Name: 47e33e932ae5f182a35c2f5d69b69ce36a78f95b847b8c790b344e58efc37563

--- tests/help/help.nim
+++ tests/help/help.nim
@@ -4,54 +4,59 @@ discard """
 import std/[db_sqlite, tables]
 import ../../src/[aliases, commandslist, directorypath, help, lstring, nish, resultcode]
 
-let db =  startDb("test.db".DirectoryPath)
-assert db != nil
+let db = startDb("test.db".DirectoryPath)
+assert db != nil, "Failed to initialize the database."
 var commands = newTable[string, CommandData]()
 
 initHelp(db, commands)
-assert commands.len() == 2
+assert commands.len() == 2, "Failed to initialize the help system."
 
 discard deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db)
 assert addHelpEntry(initLimitedString(capacity = 4, text = "test"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help", false, db) == QuitSuccess
+    capacity = 4, text = "test"), "test help", false, db) == QuitSuccess, "Failed to add a new help entry."
 assert addHelpEntry(initLimitedString(capacity = 4, text = "test"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help", false, db) == QuitFailure
+    capacity = 4, text = "test"), "test help", false, db) == QuitFailure, "Failed to not add an existing help entry."
 
 
 discard deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db)
 assert addHelpEntry(initLimitedString(capacity = 4, text = "test"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help", false, db) == QuitSuccess
-assert deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db) == QuitSuccess
-assert deleteHelpEntry(initLimitedString(capacity = 4, text = "asdd"), db) == QuitFailure
-assert deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db) == QuitFailure
+    capacity = 4, text = "test"), "test help", false, db) == QuitSuccess, "Failed to readd a help entry."
+assert deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db) ==
+    QuitSuccess, "Failed to delete a help entry."
+assert deleteHelpEntry(initLimitedString(capacity = 4, text = "asdd"), db) ==
+    QuitFailure, "Failed to not delete a non-exisiting help entry."
+assert deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db) ==
+    QuitFailure, "Failed to not delete a previously deleted help entry."
 
-assert updateHelp(db) == QuitSuccess
+assert updateHelp(db) == QuitSuccess, "Failed to updated the help system."
 
 db.exec(sql("DELETE FROM help"))
-assert readHelpFromFile(db) == QuitSuccess
-assert readHelpFromFile(db) == QuitFailure
+assert readHelpFromFile(db) == QuitSuccess, "Failed to read the help content from a file."
+assert readHelpFromFile(db) == QuitFailure, "Failed to not read again the help content from the same file."
 
-assert showHelp(initLimitedString(capacity = 12, text = "alias"), db) == QuitSuccess
-assert showHelp(initLimitedString(capacity = 9, text = "srewfdsfs"), db) == QuitFailure
+assert showHelp(initLimitedString(capacity = 12, text = "alias"), db) ==
+    QuitSuccess, "Failed to show an existing help entry."
+assert showHelp(initLimitedString(capacity = 9, text = "srewfdsfs"), db) ==
+    QuitFailure, "Failed to not show a non-existing help entry."
 
-assert showHelpList("alias", aliasesCommands) == QuitSuccess
+assert showHelpList("alias", aliasesCommands) == QuitSuccess, "Failed to show a list of subcommands."
 
 assert showUnknownHelp(initLimitedString(capacity = 7, text = "command"),
     initLimitedString(capacity = 10, text = "subcommand"), initLimitedString(
-    capacity = 8, text = "helptype")) == QuitFailure
+    capacity = 8, text = "helptype")) == QuitFailure, "Failed to show the info about unknown help command."
 
 discard deleteHelpEntry(initLimitedString(capacity = 4, text = "test"), db)
 assert addHelpEntry(initLimitedString(capacity = 4, text = "test"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help", false, db) == QuitSuccess
+    capacity = 4, text = "test"), "test help", false, db) == QuitSuccess, "Failed to readd an existing help entry."
 assert updateHelpEntry(initLimitedString(capacity = 4, text = "test"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help2", db, false) == QuitSuccess
+    capacity = 4, text = "test"), "test help2", db, false) == QuitSuccess, "Failed to update an existing help entry."
 assert updateHelpEntry(initLimitedString(capacity = 4, text = "asdd"),
     initLimitedString(capacity = 10, text = "test topic"), initLimitedString(
-    capacity = 4, text = "test"), "test help2", db, false) == QuitFailure
+    capacity = 4, text = "test"), "test help2", db, false) == QuitFailure, "Failed to not update a non-existing help entry."
 
 quitShell(ResultCode(QuitSuccess), db)
