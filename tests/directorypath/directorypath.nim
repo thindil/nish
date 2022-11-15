8b2e3c8b4be65f87f5e1159f7611600f8dac1e5b
thindil <thindil@laeran.pl>
2022-11-15 05:12:38 +0000

    tests: added messages to asserts
    FossilOrigin-Name: 48cc79dc3e6009e99149cd62e213b82d58789e1db2fba53e94ff089c49f584a1

--- tests/directorypath/directorypath.nim
+++ tests/directorypath/directorypath.nim
@@ -6,15 +6,15 @@ import ../../src/directorypath
 
 let path: DirectoryPath = "/test/path".DirectoryPath
 
-assert path != "test2"
-assert path == "/test/path"
+assert path != "test2", "Failed to compare different paths."
+assert path == "/test/path", "Failed to compare the same paths."
 
-assert path & "/test" == "/test/path/test"
-assert "/new" & path == "/new/test/path"
+assert path & "/test" == "/test/path/test", "Failed to append a string to a path."
+assert "/new" & path == "/new/test/path", "Failed to prepend a string to a path."
 
-assert path.find("e".DirectoryPath) == 2
-assert path.find("z".DirectoryPath) == -1
+assert path.find("e".DirectoryPath) == 2, "Failed to find a substring in a path."
+assert path.find("z".DirectoryPath) == -1, "Failed to not find a substring in a path."
 
-assert path.len() == 10
+assert path.len() == 10, "Failed to get the length of a path."
 
-assert $path == "/test/path"
+assert $path == "/test/path", "Failed to convert a path to a string."
