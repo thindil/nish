2c4da44f49f89c5f9317264e2ffc7095f990e28d
thindil <thindil@laeran.pl>
2022-11-17 05:11:13 +0000

    tests: added messages to asserts
    FossilOrigin-Name: 89344fe71c141a5cdd88d3227f8ec630ea728325d8043e7268191de033e0fba2

--- tests/resultcode/resultcode.nim
+++ tests/resultcode/resultcode.nim
@@ -5,6 +5,6 @@ discard """
 import ../../src/resultcode
 
 let code: ResultCode = QuitSuccess.ResultCode
-assert code == QuitSuccess
+assert code == QuitSuccess, "Failed to compare ResultCode to int."
 
-assert $code == $QuitSuccess
+assert $code == $QuitSuccess, "Failed to convert ResultCode to string."
