013e2dd2e660a0dc77aecdef166f1944d56b9974
thindil <thindil@laeran.pl>
2022-11-17 05:10:52 +0000

    tests: added messages to asserts
    FossilOrigin-Name: ffad65858fd107a8bb8b4caa6d79c073d5f28f2b86295af9396ca1d9b01518c1

--- tests/options/options.nim
+++ tests/options/options.nim
@@ -5,36 +5,39 @@ discard """
 import std/tables
 import ../../src/[commandslist, directorypath, history, lstring, nish, options, resultcode]
 
-let db =  startDb("test.db".DirectoryPath)
-assert db != nil
+let db = startDb("test.db".DirectoryPath)
+assert db != nil, "Failed to initialize the database."
 var commands = newTable[string, CommandData]()
 discard initHistory(db, commands)
 
 initOptions(commands)
 assert commands.len() > 0
 
-assert getOption(initLimitedString(capacity = 13, text = "historyLength"), db).len() > 0
-assert getOption(initLimitedString(capacity = 10, text = "werweewfwe"), db).len() == 0
+assert getOption(initLimitedString(capacity = 13, text = "historyLength"),
+    db).len() > 0, "Failed to get value of an option."
+assert getOption(initLimitedString(capacity = 10, text = "werweewfwe"), db).len() ==
+    0, "Failed to not get a value of a non-existing option."
 
 let optionName = initLimitedString(capacity = 10, text = "testOption")
-setOption(optionName = optionName, value = initLimitedString(capacity = 3, text = "200"), db = db)
-assert deleteOption(optionName, db) == QuitSuccess
-assert getOption(optionName, db).len() == 0
+setOption(optionName = optionName, value = initLimitedString(capacity = 3,
+    text = "200"), db = db)
+assert deleteOption(optionName, db) == QuitSuccess, "Failed to delete an option."
+assert getOption(optionName, db).len() == 0, "Failed to not get a deleted option."
 
 setOption(optionName = initLimitedString(capacity = 13, text = "historyLength"),
     value = initLimitedString(capacity = 3, text = "100"), db = db)
 assert getOption(initLimitedString(capacity = 13, text = "historyLength"),
-    db) == "100"
+    db) == "100", "Failed to set a value for an option."
 
 assert setOptions(initLimitedString(capacity = 22,
-    text = "set historyLength 1000"), db) == QuitSuccess
+    text = "set historyLength 1000"), db) == QuitSuccess, "Failed to set a value for an option."
 assert getOption(initLimitedString(capacity = 13, text = "historyLength"),
-    db) == "1000"
+    db) == "1000", "Failed to get a new value of an option."
 
 assert resetOptions(initLimitedString(capacity = 19,
-    text = "reset historyLength"), db) == QuitSuccess
+    text = "reset historyLength"), db) == QuitSuccess, "Failed to reset a value for an option."
 assert getOption(initLimitedString(capacity = 13, text = "historyLength"),
-    db) == "500"
+    db) == "500", "Failed to get a reseted value for an option."
 
 assert showOptions(db) == QuitSuccess
 
