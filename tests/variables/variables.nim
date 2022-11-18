7e84def6aba6044048320633dc4cbea7cee6551c
thindil <thindil@laeran.pl>
2022-11-18 05:36:16 +0000

    tests: added messages to asserts
    FossilOrigin-Name: b863f9014ece24b83c03228ae9a4cdbd881d88edce57889a11e338be1896215c

--- tests/variables/variables.nim
+++ tests/variables/variables.nim
@@ -5,10 +5,11 @@ discard """
 import std/[db_sqlite, os, strutils, tables]
 import ../../src/[commandslist, directorypath, lstring, nish, resultcode, variables]
 
-assert buildQuery("/".DirectoryPath, "name") == "SELECT name FROM variables WHERE path='/' ORDER BY id ASC"
+assert buildQuery("/".DirectoryPath, "name") ==
+    "SELECT name FROM variables WHERE path='/' ORDER BY id ASC", "Failed to build SQL query."
 
 let db = startDb("test.db".DirectoryPath)
-assert db != nil
+assert db != nil, "Failed to initialize the database."
 var commands = newTable[string, CommandData]()
 
 initVariables(db, commands)
@@ -26,26 +27,34 @@ if parseInt(db.getValue(sql"SELECT COUNT(*) FROM variables")) == 1:
     quit QuitFailure
 
 setVariables("/home".DirectoryPath, db)
-assert getEnv("TESTS") == "test_variable"
-assert not existsEnv("TESTS2")
+assert getEnv("TESTS") == "test_variable", "Failed to get value of a variable."
+assert not existsEnv("TESTS2"), "Failed to check if variable not exists."
 
-assert listVariables(initLimitedString(capacity = 4, text = "list"), db) == QuitSuccess
-assert listVariables(initLimitedString(capacity = 8, text = "list all"), db) == QuitSuccess
-assert listVariables(initLimitedString(capacity = 8, text = "werwerew"), db) == QuitSuccess
+assert listVariables(initLimitedString(capacity = 4, text = "list"), db) ==
+    QuitSuccess, "Failed to show the list of available variables."
+assert listVariables(initLimitedString(capacity = 8, text = "list all"), db) ==
+    QuitSuccess, "Failed to show the list of all variables."
+assert listVariables(initLimitedString(capacity = 8, text = "werwerew"), db) ==
+    QuitSuccess, "Failed to show the list of available variables."
 
 assert deleteVariable(initLimitedString(capacity = 10, text = "delete 123"),
-    db) == QuitFailure
+    db) == QuitFailure, "Failed to not delete a non-existing variable."
 assert deleteVariable(initLimitedString(capacity = 10, text = "delete sdf"),
-    db) == QuitFailure
-assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) == QuitSuccess
-assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) == QuitFailure
+    db) == QuitFailure, "Failed to not delete a non-existing variable with invalid index."
+assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) ==
+    QuitSuccess, "Failed to delete a variable."
+assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) ==
+    QuitFailure, "Failed to delete a deleted variable."
 
-assert setCommand(initLimitedString(capacity = 13, text = "test=test_val")) == QuitSuccess
-assert getEnv("test") == "test_val"
+assert setCommand(initLimitedString(capacity = 13, text = "test=test_val")) ==
+    QuitSuccess, "Failed to set a variable."
+assert getEnv("test") == "test_val", "Failed to get a value of a variable."
 
-assert unsetCommand(initLimitedString(capacity = 4, text = "test")) == QuitSuccess
-assert getEnv("test") == ""
-assert unsetCommand(initLimitedString(capacity = 4, text = "test")) == QuitSuccess
+assert unsetCommand(initLimitedString(capacity = 4, text = "test")) ==
+    QuitSuccess, "Failed to unset a variable."
+assert getEnv("test") == "", "Failed to not get a value of a non-existing variable."
+assert unsetCommand(initLimitedString(capacity = 4, text = "test")) ==
+    QuitSuccess, "Failed to unset a non-existing variable."
 
 quitShell(ResultCode(QuitSuccess), db)
 
