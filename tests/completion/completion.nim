62a558f1cc13748df06c525530b4f3e98769aae5
thindil <thindil@laeran.pl>
2022-11-29 05:38:58 +0000

    tests: updated unit tests for completions module
    FossilOrigin-Name: 42d98e52df8b6f28b0b48a07ed4e74dd68b91e9271a3ddc95ef780776eccc23d

--- tests/completion/completion.nim
+++ tests/completion/completion.nim
@@ -2,12 +2,29 @@ discard """
   exitcode: 0
 """
 
-import os
-import ../../src/completion
+import std/[db_sqlite, os, strutils, tables]
+import ../../src/[aliases, completion, commandslist, directorypath, lstring, nish]
 
-var completions: seq[string]
+let db = startDb("test.db".DirectoryPath)
+assert db != nil, "No connection to database."
+var
+  myaliases = newOrderedTable[LimitedString, int]()
+  commands = newTable[string, CommandData]()
+  completions: seq[string]
+if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
+  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+      "tests", "/", 1, "ls -a", "Test alias.") == -1:
+    quit("Can't add test alias.")
+if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 1:
+  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+      "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
+    quit("Can't add the second test alias.")
+initAliases(db, myaliases, commands)
 
 open("sometest.txt", fmWrite).close()
 getDirCompletion("somete", completions)
 removeFile("sometest.txt")
-assert completions == @["sometest.txt"], "Failed to get Tab completion."
+assert completions == @["sometest.txt"], "Failed to get Tab completion for a file."
+
+getCommandCompletion("exi", completions, myaliases)
+assert completions[1] == "exit", "Failed to get Tab completion for a command."
