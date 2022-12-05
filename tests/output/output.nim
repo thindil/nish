e15b2291d861d4af63b99bb9a5dec31ce6ec085d
thindil <thindil@laeran.pl>
2022-12-05 05:00:20 +0000

    tests: removed test for getIndent procedure
    FossilOrigin-Name: b5d419643336c17c60b127c7959806e34006493e1fd41e32e2f9eef08f4b47f5

--- tests/output/output.nim
+++ tests/output/output.nim
@@ -2,11 +2,9 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[columnamount, output, resultcode]
+import ../../src/[output, resultcode]
 
 assert showError("test error") == QuitFailure, "Failed to show error message."
-
-assert getIndent(1.ColumnAmount) == 1
 
 showFormHeader("test header")
 
