737594d56b03eaadf9531bb9bd6b70c6e5dc18a7
thindil <thindil@laeran.pl>
2022-12-12 05:27:59 +0000

    style: removed unnecessary parenthesis
    FossilOrigin-Name: e90400d888041aa06063b5bd4acbdcd3cea12771c06884d30c987efe2423ba4f

--- tests/prompt/prompt.nim
+++ tests/prompt/prompt.nim
@@ -7,5 +7,5 @@ import ../../src/[nish, directorypath, prompt, resultcode]
 let db = startDb("test.db".DirectoryPath)
 assert db != nil, "Failed to initialize the database."
 
-assert getFormattedDir().len() > 0, "Failed to get formatted current directory path."
+assert getFormattedDir().len > 0, "Failed to get formatted current directory path."
 showPrompt(true, "ls -a", ResultCode(QuitSuccess), db)
