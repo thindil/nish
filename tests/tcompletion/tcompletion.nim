96a6fcdfb78119b976fce476b7225f5c75f3a03c
thindil <thindil@laeran.pl>
2022-12-23 06:17:03 +0000

    tests: added blocks to tests
    FossilOrigin-Name: 51f59930d938a8f9228c965a958bc49b2f8afd25cc65cb5413c88c8f1a01837b

--- tests/tcompletion/tcompletion.nim
+++ tests/tcompletion/tcompletion.nim
@@ -3,28 +3,32 @@ discard """
 """
 
 import std/[db_sqlite, os, strutils, tables]
-import ../../src/[aliases, completion, commandslist, directorypath, lstring, nish]
+import ../../src/[aliases, completion, commandslist, directorypath, lstring,
+    nish, resultcode]
 
-let db = startDb("test.db".DirectoryPath)
-assert db != nil, "No connection to database."
-var
-  myaliases = newOrderedTable[LimitedString, int]()
-  commands = newTable[string, CommandData]()
-  completions: seq[string]
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
-  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-      "tests", "/", 1, "ls -a", "Test alias.") == -1:
-    quit("Can't add test alias.")
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 1:
-  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-      "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
-    quit("Can't add the second test alias.")
-initAliases(db, myaliases, commands)
+block:
+  let db = startDb("test5.db".DirectoryPath)
+  assert db != nil, "No connection to database."
+  var
+    myaliases = newOrderedTable[LimitedString, int]()
+    commands = newTable[string, CommandData]()
+    completions: seq[string]
+  if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests", "/", 1, "ls -a", "Test alias.") == -1:
+      quit("Can't add test alias.")
+  if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 1:
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
+      quit("Can't add the second test alias.")
+  initAliases(db, myaliases, commands)
 
-open("sometest.txt", fmWrite).close
-getDirCompletion("somete", completions, db)
-removeFile("sometest.txt")
-assert completions == @["sometest.txt"], "Failed to get Tab completion for a file."
+  open("sometest.txt", fmWrite).close
+  getDirCompletion("somete", completions, db)
+  removeFile("sometest.txt")
+  assert completions == @["sometest.txt"], "Failed to get Tab completion for a file."
 
-getCommandCompletion("exi", completions, myaliases, commands, db)
-assert completions[1] == "exit", "Failed to get Tab completion for a command."
+  getCommandCompletion("exi", completions, myaliases, commands, db)
+  assert completions[1] == "exit", "Failed to get Tab completion for a command."
+
+  quitShell(ResultCode(QuitSuccess), db)
