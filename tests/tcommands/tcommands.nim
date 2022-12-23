04d804afd980a6d3eda9321475a34ff795392f7d
thindil <thindil@laeran.pl>
2022-12-23 06:16:44 +0000

    tests: added blocks to tests
    FossilOrigin-Name: 9392037c2866bb9e12a2a4ef2ec4b3749bcca0f6e5269727b987ed6f5950f003

--- tests/tcommands/tcommands.nim
+++ tests/tcommands/tcommands.nim
@@ -1,26 +1,28 @@
 discard """
   exitcode: 0
+  outputsub: "/adfwerewtr"
 """
 
 import std/[db_sqlite, strutils, tables]
 import ../../src/[commands, directorypath, lstring, nish, resultcode]
 
-let db = startDb("test.db".DirectoryPath)
-assert db != nil, "Failed to initialize database."
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
-  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-      "tests", "/", 1, "ls -a", "Test alias.") == -1:
-    quit("Can't add test alias.", QuitFailure)
-  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
-      "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
-    quit("Can't add test2 alias.", QuitFailure)
-var myaliases = newOrderedTable[LimitedString, int]()
+block:
+  let db = startDb("test3.db".DirectoryPath)
+  assert db != nil, "Failed to initialize database."
+  if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests", "/", 1, "ls -a", "Test alias.") == -1:
+      quit("Can't add test alias.", QuitFailure)
+    if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description) VALUES (?, ?, ?, ?, ?)",
+        "tests2", "/", 0, "ls -a", "Test alias 2.") == -1:
+      quit("Can't add test2 alias.", QuitFailure)
+  var myaliases = newOrderedTable[LimitedString, int]()
 
-assert cdCommand("/".DirectoryPath, myaliases, db) == QuitSuccess, "Failed to enter an existing directory."
-assert cdCommand("/adfwerewtr".DirectoryPath, myaliases, db) == QuitFailure, "Failed to not enter a non-existing directory."
+  assert cdCommand("/".DirectoryPath, myaliases, db) == QuitSuccess, "Failed to enter an existing directory."
+  assert cdCommand("/adfwerewtr".DirectoryPath, myaliases, db) == QuitFailure, "Failed to not enter a non-existing directory."
 
-assert changeDirectory("..".DirectoryPath, myaliases, db) == QuitSuccess, "Failed to change the working directory."
-assert changeDirectory("/adfwerewtr".DirectoryPath, myaliases, db) ==
-    QuitFailure, "Failed to not change the working directory."
+  assert changeDirectory("..".DirectoryPath, myaliases, db) == QuitSuccess, "Failed to change the working directory."
+  assert changeDirectory("/adfwerewtr".DirectoryPath, myaliases, db) ==
+      QuitFailure, "Failed to not change the working directory."
 
-quitShell(ResultCode(QuitSuccess), db)
+  quitShell(ResultCode(QuitSuccess), db)
