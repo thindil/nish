2b0800432acc4208d1e1d2990f37bae9d3d39767
thindil <thindil@laeran.pl>
2022-12-24 04:48:51 +0000

    tests: added blocks to tests
    FossilOrigin-Name: b7986cb7b22a5eb1753629018000d370dbc91ee10cce27b811528d1d172940e9

--- tests/thistory/thistory.nim
+++ tests/thistory/thistory.nim
@@ -5,31 +5,32 @@ discard """
 import std/[db_sqlite, tables]
 import ../../src/[commandslist, directorypath, history, lstring, nish, resultcode]
 
-let db = startDb("test.db".DirectoryPath)
-assert db != nil, "Failed to initialize the database."
-var commands = newTable[string, CommandData]()
-var amount = initHistory(db, commands)
-if amount == 0:
-  if db.tryInsertID(sql"INSERT INTO history (command, amount, lastused) VALUES (?, 1, datetime('now'))",
-      "alias delete") == -1:
-    quit QuitFailure
+block:
+  let db = startDb("test8.db".DirectoryPath)
+  assert db != nil, "Failed to initialize the database."
+  var commands = newTable[string, CommandData]()
+  var amount = initHistory(db, commands)
+  if amount == 0:
+    if db.tryInsertID(sql"INSERT INTO history (command, amount, lastused) VALUES (?, 1, datetime('now'))",
+        "alias delete") == -1:
+      quit QuitFailure
 
-assert getHistory(1, db) == "alias delete", "Failed to get the history entry."
+  assert getHistory(1, db) == "alias delete", "Failed to get the history entry."
 
-amount = historyLength(db)
-assert updateHistory("test comm", db) == amount + 1, "Failed to update the history."
+  amount = historyLength(db)
+  assert updateHistory("test comm", db) == amount + 1, "Failed to update the history."
 
-assert historyLength(db) > 0
+  assert historyLength(db) > 0
 
-assert showHistory(db, initLimitedString(capacity = 4, text = "list")) ==
-    QuitSuccess, "Failed to show the history."
+  assert showHistory(db, initLimitedString(capacity = 4, text = "list")) ==
+      QuitSuccess, "Failed to show the history."
 
-assert findInHistory(db, initLimitedString(capacity = 7, text = "find te")) ==
-    QuitSuccess, "Failed to find a term in the history."
-assert findInHistory(db, initLimitedString(capacity = 8, text = "find asd")) ==
-    QuitFailure, "Failed to not find a term in the history."
+  assert findInHistory(db, initLimitedString(capacity = 7, text = "find te")) ==
+      QuitSuccess, "Failed to find a term in the history."
+  assert findInHistory(db, initLimitedString(capacity = 8, text = "find asd")) ==
+      QuitFailure, "Failed to not find a term in the history."
 
-assert clearHistory(db) == 0, "Failed to clear the history"
-assert historyLength(db) == 0, "Failed to get the histry length"
+  assert clearHistory(db) == 0, "Failed to clear the history"
+  assert historyLength(db) == 0, "Failed to get the histry length"
 
-quitShell(ResultCode(QuitSuccess), db)
+  quitShell(ResultCode(QuitSuccess), db)
