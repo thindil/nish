35d79410e9d9c868b34f81ab08d6edad550f8513
thindil <thindil@laeran.pl>
2022-12-24 04:48:44 +0000

    tests: added blocks to tests
    FossilOrigin-Name: 31362d6a7efbabac7588ea6357e1ca36784dc8550e2eb4f7631524fd7df6206a

--- tests/thighlight/thighlight.nim
+++ tests/thighlight/thighlight.nim
@@ -6,13 +6,15 @@ import std/tables
 import ../../src/[directorypath, commandslist, highlight, lstring, nish,
     resultcode, constants]
 
-let db = startDb("test.db".DirectoryPath)
-assert db != nil, "Failed to initialize the database"
-var
-  myaliases = newOrderedTable[LimitedString, int]()
-  commands = newTable[string, CommandData]()
-  inputString: UserInput = initLimitedString(4, "test")
+block:
+  let db = startDb("test7.db".DirectoryPath)
+  assert db != nil, "Failed to initialize the database"
+  var
+    myaliases = newOrderedTable[LimitedString, int]()
+    commands = newTable[string, CommandData]()
+    inputString: UserInput = initLimitedString(4, "test")
 
-highlightOutput(0, inputString, commands, myaliases, false, "",
-    QuitSuccess.ResultCode, db, 0)
+  highlightOutput(0, inputString, commands, myaliases, false, "",
+      QuitSuccess.ResultCode, db, 0)
 
+  quitShell(ResultCode(QuitSuccess), db)
