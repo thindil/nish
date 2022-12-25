351a3eacf8981e4689524503ba5ba115c2ca1950
thindil <thindil@laeran.pl>
2022-12-25 04:46:29 +0000

    tests: added blocks to tests
    FossilOrigin-Name: 76ade91471539e45c62b96051f3321e3026a8c83646563379c38fc9033f90f8d

--- tests/tdirectorypath/tdirectorypath.nim
+++ tests/tdirectorypath/tdirectorypath.nim
@@ -4,17 +4,18 @@ discard """
 
 import ../../src/directorypath
 
-let path: DirectoryPath = "/test/path".DirectoryPath
+block:
+  let path: DirectoryPath = "/test/path".DirectoryPath
 
-assert path != "test2", "Failed to compare different paths."
-assert path == "/test/path", "Failed to compare the same paths."
+  assert path != "test2", "Failed to compare different paths."
+  assert path == "/test/path", "Failed to compare the same paths."
 
-assert path & "/test" == "/test/path/test", "Failed to append a string to a path."
-assert "/new" & path == "/new/test/path", "Failed to prepend a string to a path."
+  assert path & "/test" == "/test/path/test", "Failed to append a string to a path."
+  assert "/new" & path == "/new/test/path", "Failed to prepend a string to a path."
 
-assert path.find("e".DirectoryPath) == 2, "Failed to find a substring in a path."
-assert path.find("z".DirectoryPath) == -1, "Failed to not find a substring in a path."
+  assert path.find("e".DirectoryPath) == 2, "Failed to find a substring in a path."
+  assert path.find("z".DirectoryPath) == -1, "Failed to not find a substring in a path."
 
-assert path.len == 10, "Failed to get the length of a path."
+  assert path.len == 10, "Failed to get the length of a path."
 
-assert $path == "/test/path", "Failed to convert a path to a string."
+  assert $path == "/test/path", "Failed to convert a path to a string."
