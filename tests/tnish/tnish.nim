0bb72b469a31827ac697930af10baa44bb9cfebf
thindil <thindil@laeran.pl>
2022-12-25 04:46:01 +0000

    tests: added blocks to tests
    FossilOrigin-Name: 9f6f41b60a2049d4e3f1b13bafb3c8fbc6ea0c474bd6908e0a26b731e9684371

--- tests/tnish/tnish.nim
+++ tests/tnish/tnish.nim
@@ -5,14 +5,15 @@ discard """
 import std/tables
 import ../../src/[commandslist, directorypath, history, nish, resultcode]
 
-showCommandLineHelp()
+block:
+  showCommandLineHelp()
 
-showProgramVersion()
+  showProgramVersion()
 
-let db = startDb("test.db".DirectoryPath)
-assert db != nil, "Failed to initialize the database."
-var
-    historyIndex: int
-    commands = newTable[string, CommandData]()
-historyIndex = initHistory(db, commands)
-quitShell(ResultCode(QuitSuccess), db)
+  let db = startDb("test10.db".DirectoryPath)
+  assert db != nil, "Failed to initialize the database."
+  var
+      historyIndex: int
+      commands = newTable[string, CommandData]()
+  historyIndex = initHistory(db, commands)
+  quitShell(ResultCode(QuitSuccess), db)
