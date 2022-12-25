c4e45c5e1d9ff10e52a57b2033f4024782895a86
thindil <thindil@laeran.pl>
2022-12-25 04:46:20 +0000

    tests: added blocks to tests
    FossilOrigin-Name: 32ff2853c87905bdd10a624585491330b880b16cd80fa5f240a1f7c0c4c2fe5b

--- tests/tcolumnamount/tcolumnamount.nim
+++ tests/tcolumnamount/tcolumnamount.nim
@@ -4,9 +4,10 @@ discard """
 
 import ../../src/columnamount
 
-let code: ColumnAmount = 42.ColumnAmount
-assert code == 42, "Failed to compare ColumnAmount."
+block:
+  let code: ColumnAmount = 42.ColumnAmount
+  assert code == 42, "Failed to compare ColumnAmount."
 
-assert (12.ColumnAmount / 2).int == 6, "Failed to divide ColumnAmount"
-assert (10.ColumnAmount - 5) == 5, "Failed to substract ColumnAmount"
-assert (5.ColumnAmount * 2) == 10, "Failed to multiply ColumnAmount"
+  assert (12.ColumnAmount / 2).int == 6, "Failed to divide ColumnAmount"
+  assert (10.ColumnAmount - 5) == 5, "Failed to substract ColumnAmount"
+  assert (5.ColumnAmount * 2) == 10, "Failed to multiply ColumnAmount"
