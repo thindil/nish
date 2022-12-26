ccdf324f19394f9d364af856a8c0a308c7d0e224
thindil <thindil@laeran.pl>
2022-12-26 04:52:19 +0000

    tests: added blocks to tests
    FossilOrigin-Name: 1779f2a68fb5a77bf139c528cbc3edf7e423322949c5f02adf3da97e89cc77d4

--- tests/tvariables/tvariables.nim
+++ tests/tvariables/tvariables.nim
@@ -5,56 +5,57 @@ discard """
 import std/[db_sqlite, os, strutils, tables]
 import ../../src/[commandslist, directorypath, lstring, nish, resultcode, variables]
 
-assert buildQuery("/".DirectoryPath, "name") ==
-    "SELECT name FROM variables WHERE path='/' ORDER BY id ASC", "Failed to build SQL query."
+block:
+  assert buildQuery("/".DirectoryPath, "name") ==
+      "SELECT name FROM variables WHERE path='/' ORDER BY id ASC", "Failed to build SQL query."
 
-let db = startDb("test.db".DirectoryPath)
-assert db != nil, "Failed to initialize the database."
-var commands = newTable[string, CommandData]()
+  let db = startDb("test.db".DirectoryPath)
+  assert db != nil, "Failed to initialize the database."
+  var commands = newTable[string, CommandData]()
 
-initVariables(db, commands)
+  initVariables(db, commands)
 
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM variables")) == 0:
-  if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
-      "TESTS", "/", 1, "test_variable", "Test variable.") == -1:
-    quit QuitFailure
-  if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
-      "TESTS2", "/", 0, "test_variable2", "Test variable 2.") == -1:
-    quit QuitFailure
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM variables")) == 1:
-  if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
-      "TESTS2", "/", 0, "test_variable2", "Test variable 2.") == -1:
-    quit QuitFailure
+  if parseInt(db.getValue(sql"SELECT COUNT(*) FROM variables")) == 0:
+    if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
+        "TESTS", "/", 1, "test_variable", "Test variable.") == -1:
+      quit QuitFailure
+    if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
+        "TESTS2", "/", 0, "test_variable2", "Test variable 2.") == -1:
+      quit QuitFailure
+  if parseInt(db.getValue(sql"SELECT COUNT(*) FROM variables")) == 1:
+    if db.tryInsertID(sql"INSERT INTO variables (name, path, recursive, value, description) VALUES (?, ?, ?, ?, ?)",
+        "TESTS2", "/", 0, "test_variable2", "Test variable 2.") == -1:
+      quit QuitFailure
 
-setVariables("/home".DirectoryPath, db)
-assert getEnv("TESTS") == "test_variable", "Failed to get value of a variable."
-assert not existsEnv("TESTS2"), "Failed to check if variable not exists."
+  setVariables("/home".DirectoryPath, db)
+  assert getEnv("TESTS") == "test_variable", "Failed to get value of a variable."
+  assert not existsEnv("TESTS2"), "Failed to check if variable not exists."
 
-assert listVariables(initLimitedString(capacity = 4, text = "list"), db) ==
-    QuitSuccess, "Failed to show the list of available variables."
-assert listVariables(initLimitedString(capacity = 8, text = "list all"), db) ==
-    QuitSuccess, "Failed to show the list of all variables."
-assert listVariables(initLimitedString(capacity = 8, text = "werwerew"), db) ==
-    QuitSuccess, "Failed to show the list of available variables."
+  assert listVariables(initLimitedString(capacity = 4, text = "list"), db) ==
+      QuitSuccess, "Failed to show the list of available variables."
+  assert listVariables(initLimitedString(capacity = 8, text = "list all"), db) ==
+      QuitSuccess, "Failed to show the list of all variables."
+  assert listVariables(initLimitedString(capacity = 8, text = "werwerew"), db) ==
+      QuitSuccess, "Failed to show the list of available variables."
 
-assert deleteVariable(initLimitedString(capacity = 10, text = "delete 123"),
-    db) == QuitFailure, "Failed to not delete a non-existing variable."
-assert deleteVariable(initLimitedString(capacity = 10, text = "delete sdf"),
-    db) == QuitFailure, "Failed to not delete a non-existing variable with invalid index."
-assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) ==
-    QuitSuccess, "Failed to delete a variable."
-assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) ==
-    QuitFailure, "Failed to delete a deleted variable."
+  assert deleteVariable(initLimitedString(capacity = 10, text = "delete 123"),
+      db) == QuitFailure, "Failed to not delete a non-existing variable."
+  assert deleteVariable(initLimitedString(capacity = 10, text = "delete sdf"),
+      db) == QuitFailure, "Failed to not delete a non-existing variable with invalid index."
+  assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) ==
+      QuitSuccess, "Failed to delete a variable."
+  assert deleteVariable(initLimitedString(capacity = 8, text = "delete 2"), db) ==
+      QuitFailure, "Failed to delete a deleted variable."
 
-assert setCommand(initLimitedString(capacity = 13, text = "test=test_val")) ==
-    QuitSuccess, "Failed to set a variable."
-assert getEnv("test") == "test_val", "Failed to get a value of a variable."
+  assert setCommand(initLimitedString(capacity = 13, text = "test=test_val")) ==
+      QuitSuccess, "Failed to set a variable."
+  assert getEnv("test") == "test_val", "Failed to get a value of a variable."
 
-assert unsetCommand(initLimitedString(capacity = 4, text = "test")) ==
-    QuitSuccess, "Failed to unset a variable."
-assert getEnv("test") == "", "Failed to not get a value of a non-existing variable."
-assert unsetCommand(initLimitedString(capacity = 4, text = "test")) ==
-    QuitSuccess, "Failed to unset a non-existing variable."
+  assert unsetCommand(initLimitedString(capacity = 4, text = "test")) ==
+      QuitSuccess, "Failed to unset a variable."
+  assert getEnv("test") == "", "Failed to not get a value of a non-existing variable."
+  assert unsetCommand(initLimitedString(capacity = 4, text = "test")) ==
+      QuitSuccess, "Failed to unset a non-existing variable."
 
-quitShell(ResultCode(QuitSuccess), db)
+  quitShell(ResultCode(QuitSuccess), db)
 
