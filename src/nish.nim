da381e7ca05577337fccdb300159ccf1e2f3794f
thindil <thindil@laeran.pl>
2023-08-20 04:02:29 +0000

    docs: removed mail from headers
    FossilOrigin-Name: 202796947dd991a16e399080f02f7359d35f068315aa8f0188a1b56e7b4c1d90

--- src/nish.nim
+++ src/nish.nim
@@ -1,4 +1,4 @@
-# Copyright © 2021-2023 Bartek Jasicki <thindil@laeran.pl>
+# Copyright © 2021-2023 Bartek Jasicki
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
