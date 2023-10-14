051af243248aab99ff4c898149f99f90c4e07a8f
thindil <thindil@laeran.pl>
2023-10-14 04:14:20 +0000

    tests: updated unit test for commands
    FossilOrigin-Name: 9e14a9a141250b7fc8bc6dda8e8c2695953a8a6396cde5a7f8e3be7f5f9fae14

--- tests/tcommands/tcommands.nim
+++ tests/tcommands/tcommands.nim
@@ -4,21 +4,25 @@ discard """
 """
 
 import std/[strutils, tables]
-when (NimMajor, NimMinor, NimPatch) >= (1, 7, 3):
-  import db_connector/db_sqlite
-else:
-  import std/db_sqlite
-import ../../src/[commands, directorypath, lstring, nish, resultcode]
+import ../../src/[aliases, commands, directorypath, lstring, nish, resultcode]
+import norm/sqlite
 
 let db = startDb("test3.db".DirectoryPath)
 assert db != nil, "Failed to initialize database."
-if parseInt(db.getValue(sql"SELECT COUNT(*) FROM aliases")) == 0:
-  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description, output) VALUES (?, ?, ?, ?, ?, ?)",
-      "tests", "/", 1, "ls -a", "Test alias.", "output") == -1:
-    quit("Can't add test alias.", QuitFailure)
-  if db.tryInsertID(sql"INSERT INTO aliases (name, path, recursive, commands, description, output) VALUES (?, ?, ?, ?, ?, ?)",
-      "tests2", "/", 0, "ls -a", "Test alias 2.", "output") == -1:
-    quit("Can't add test2 alias.", QuitFailure)
+if db.count(Alias) == 0:
+  try:
+    var alias = newAlias(name = "tests", path = "/", recursive = true,
+        commands = "ls -a", description = "Test alias.", output = "output")
+    db.insert(alias)
+  except:
+    quit("Can't add test alias.")
+  try:
+    var testAlias2 = newAlias(name = "tests2", path = "/", recursive = false,
+        commands = "ls -a", description = "Test alias 2.", output = "output")
+    db.insert(testAlias2)
+  except:
+    quit("Can't add the second test alias.")
+
 var myaliases = newOrderedTable[LimitedString, int]()
 
 assert cdCommand("/".DirectoryPath, myaliases, db) == QuitSuccess, "Failed to enter an existing directory."
