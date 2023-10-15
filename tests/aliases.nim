3ed775e32b134784f7214b7ba8152490ebe12e5b
thindil <thindil@laeran.pl>
2023-10-15 04:04:45 +0000

    tests: moved unit test for aliases module
    FossilOrigin-Name: 3a0d4a7cb6bfacf9e7c44e04c647831868991ef066f98f5a14ca17ac02182237

---
+++ tests/aliases.nim
@@ -0,0 +1,66 @@
+import std/[os, strutils, tables]
+import ../src/[aliases, db, directorypath, commandslist, lstring, resultcode]
+import contracts, unittest2
+import norm/sqlite
+
+suite "Unit tests for aliases module":
+
+  let db = startDb("test2.db".DirectoryPath)
+  assert db != nil, "No connection to database."
+  var
+    myaliases = newOrderedTable[LimitedString, int]()
+    commands = newTable[string, CommandData]()
+
+  if db.count(Alias) == 0:
+    var alias = newAlias(name = "tests", path = "/", recursive = true,
+          commands = "ls -a", description = "Test alias.", output = "output")
+    db.insert(alias)
+    var testAlias2 = newAlias(name = "tests2", path = "/", recursive = false,
+        commands = "ls -a", description = "Test alias 2.", output = "output")
+    db.insert(testAlias2)
+
+  test "initAliases":
+    initAliases(db, myaliases, commands)
+    check:
+      myaliases.len == 1
+
+  test "deleteAlias":
+    check:
+      deleteAlias(initLimitedString(capacity = 8, text = "delete 2"),
+        myaliases, db) == QuitSuccess
+      db.count(Alias) == 1
+      deleteAlias(initLimitedString(capacity = 9, text = "delete 22"),
+        myaliases, db) == QuitFailure
+    var testAlias2 = newAlias(name = "tests2", path = "/", recursive = false,
+      commands = "ls -a", description = "Test alias 2.", output = "output")
+    db.insert(testAlias2)
+    unittest2.require:
+      db.count(Alias) == 2
+
+  test "setAliases":
+    myaliases.setAliases(getCurrentDir().DirectoryPath, db)
+    check:
+      execAlias(emptyLimitedString(), "tests", myaliases, db) == QuitSuccess
+      execAlias(emptyLimitedString(), "tests2", myaliases, db) == QuitFailure
+
+  test "listAliases":
+    check:
+      db.count(Alias) == 2
+      listAliases(initLimitedString(capacity = 4, text = "list"), myaliases,
+        db) == QuitSuccess
+      listAliases(initLimitedString(capacity = 8, text = "list all"),
+        myaliases, db) == QuitSuccess
+    try:
+      check:
+        listAliases(initLimitedString(capacity = 8, text = "werwerew"),
+          myaliases, db) == QuitSuccess
+    except PreConditionDefect:
+      discard
+
+  test "newAlias":
+    let newAlias = newAlias(name = "ala")
+    check:
+      newAlias.name == "ala"
+
+  suiteTeardown:
+    quitShell(QuitSuccess.ResultCode, db)
