0b5d4e70a81f48bef463018e75f1bb0a5dff5aaa
thindil <thindil@laeran.pl>
2023-10-19 04:16:42 +0000

    tests: updated unit tests for aliases module
    FossilOrigin-Name: c296d465d4057d8ae0f5b764d59baa4e3531e434eeac3d623791d37ae91f3662

--- tests/aliases.nim
+++ tests/aliases.nim
@@ -5,12 +5,15 @@ import norm/sqlite
 
 suite "Unit tests for aliases module":
 
+  checkpoint "Initializing the tests"
   let db = startDb("test2.db".DirectoryPath)
-  assert db != nil, "No connection to database."
+  unittest2.require:
+    db != nil
   var
     myaliases = newOrderedTable[LimitedString, int]()
     commands = newTable[string, CommandData]()
 
+  checkpoint "Adding testing aliases if needed"
   if db.count(Alias) == 0:
     var alias = newAlias(name = "tests", path = "/", recursive = true,
           commands = "ls -a", description = "Test alias.", output = "output")
@@ -19,45 +22,54 @@ suite "Unit tests for aliases module":
         commands = "ls -a", description = "Test alias 2.", output = "output")
     db.insert(testAlias2)
 
-  test "initAliases":
+  test "Initialization of the shell's aliases":
     initAliases(db, myaliases, commands)
     check:
       myaliases.len == 1
 
-  test "deleteAlias":
+  test "Deleting the shell's alias":
+    checkpoint "Deleting an existing alias"
     check:
       deleteAlias(initLimitedString(capacity = 8, text = "delete 2"),
         myaliases, db) == QuitSuccess
       db.count(Alias) == 1
+    checkpoint "Deleting a non-existing alias"
+    check:
       deleteAlias(initLimitedString(capacity = 9, text = "delete 22"),
         myaliases, db) == QuitFailure
+    checkpoint("Readding the test alias")
     var testAlias2 = newAlias(name = "tests2", path = "/", recursive = false,
       commands = "ls -a", description = "Test alias 2.", output = "output")
     db.insert(testAlias2)
     unittest2.require:
       db.count(Alias) == 2
 
-  test "setAliases":
+  test "Setting the shell's aliases in the current directory":
     myaliases.setAliases(getCurrentDir().DirectoryPath, db)
+    checkpoint "Checking an existing alias"
     check:
       execAlias(emptyLimitedString(), "tests", myaliases, db) == QuitSuccess
+    checkpoint "Checking a non existing alias"
+    check:
       execAlias(emptyLimitedString(), "tests2", myaliases, db) == QuitFailure
 
-  test "listAliases":
+  test "Listing the shell's aliases":
+    checkpoint "List the shell's aliases in the current directory"
     check:
       db.count(Alias) == 2
       listAliases(initLimitedString(capacity = 4, text = "list"), myaliases,
         db) == QuitSuccess
+    checkpoint "List all available the shell aliases"
+    check:
       listAliases(initLimitedString(capacity = 8, text = "list all"),
         myaliases, db) == QuitSuccess
-    try:
+    checkpoint "Check what happen when invalid argument passed to listAliases"
+    expect PreConditionDefect:
       check:
         listAliases(initLimitedString(capacity = 8, text = "werwerew"),
           myaliases, db) == QuitSuccess
-    except PreConditionDefect:
-      discard
 
-  test "newAlias":
+  test "Initializing an object of Alias type":
     let newAlias = newAlias(name = "ala")
     check:
       newAlias.name == "ala"
