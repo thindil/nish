cb762ecfd20958e5b15f5add3371879957ae08d3
thindil <thindil@laeran.pl>
2023-10-20 03:56:37 +0000

    tests: updated unit tests for constants module
    FossilOrigin-Name: b40e42087870a98238d09bce55202ca861b93f169bf0fd622be0c31dffa45386

--- tests/constants.nim
+++ tests/constants.nim
@@ -4,12 +4,14 @@ import unittest2
 
 suite "Unit tests for constant module":
 
-  test "getCurrentDirectory":
+  test "Get the current directory":
+    checkpoint "Get an existing current directory"
     check:
       getCurrentDirectory() == getCurrentDir()
     let testDir = getCurrentDir() &  DirSep & "test"
     createDir(testDir)
     setCurrentDir(testDir)
     removeDir(testDir)
+    checkpoint "Get a non-existing current directory"
     check:
       getCurrentDirectory() == getHomeDir()
