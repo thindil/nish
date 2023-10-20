dcf644038ef9c64c9fd1cfc3197ec194f0fd0aa4
thindil <thindil@laeran.pl>
2023-10-20 03:56:28 +0000

    tests: updated unit tests for completion module
    FossilOrigin-Name: 6d608f36eacf85ee124c72b3aea9594d9d74b97507c9041cfe5131f388a76acc

--- tests/completion.nim
+++ tests/completion.nim
@@ -5,35 +5,33 @@ import norm/sqlite
 
 suite "Unit tests for completion module":
 
+  checkpoint "Initializing the tests"
   let db = startDb("test5.db".DirectoryPath)
-  assert db != nil, "No connection to database."
+  require:
+    db != nil
   var
     myaliases = newOrderedTable[LimitedString, int]()
     commands = newTable[string, CommandData]()
     completions: seq[string]
+
+  checkpoint "Adding testing aliases if needed"
   if db.count(Alias) == 0:
-    try:
-      var alias = newAlias(name = "tests", path = "/", recursive = true,
-          commands = "ls -a", description = "Test alias.", output = "output")
-      db.insert(alias)
-    except:
-      quit("Can't add test alias.")
-    try:
-      var testAlias2 = newAlias(name = "tests2", path = "/", recursive = false,
-          commands = "ls -a", description = "Test alias 2.", output = "output")
-      db.insert(testAlias2)
-    except:
-      quit("Can't add the second test alias.")
+    var alias = newAlias(name = "tests", path = "/", recursive = true,
+        commands = "ls -a", description = "Test alias.", output = "output")
+    db.insert(alias)
+    var testAlias2 = newAlias(name = "tests2", path = "/", recursive = false,
+        commands = "ls -a", description = "Test alias 2.", output = "output")
+    db.insert(testAlias2)
   initAliases(db, myaliases, commands)
 
-  test "getDirCompletion":
+  test "Get completion for a file name":
     open("sometest.txt", fmWrite).close
     getDirCompletion("somete", completions, db)
     removeFile("sometest.txt")
     check:
       completions == @["sometest.txt"]
 
-  test "getCommandCompletion":
+  test "Get completion for a command":
     getCommandCompletion("exi", completions, myaliases, commands, db)
     check:
       completions[1] == "exit"
