d988f609a38815bc20e7e78fe4135e6aa93799ab
thindil <thindil@laeran.pl>
2023-10-20 03:56:48 +0000

    tests: updated unit tests for directorypath module
    FossilOrigin-Name: b0b8806238a291dddb56e6c134613d6958a0dd178388d0c02cdfaf92092d30ec

--- tests/directorypath.nim
+++ tests/directorypath.nim
@@ -5,18 +5,27 @@ suite "Unit tests for directorypath module":
   let path: DirectoryPath = "/test/path".DirectoryPath
 
   test "Compare paths":
+    checkpoint "Compare different paths"
     check:
       path != "test2"
+    checkpoint "Compare the same path"
+    check:
       path == "/test/path"
 
   test "Append to a path":
+    checkpoint "Prepend a string to a path"
     check:
       path & "/test" == "/test/path/test"
+    checkpoint "Append a string to a path"
+    check:
       "/new" & path == "/new/test/path"
 
   test "Find in a path":
+    checkpoint "Find an existing character in a path"
     check:
       path.find("e".DirectoryPath) == 2
+    checkpoint "Not find a non-existing character in a path"
+    check:
       path.find("z".DirectoryPath) == -1
 
   test "Length of a path":
