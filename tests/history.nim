0eca22c6367392a418fc8c96fe36129fc52a3777
thindil <thindil@laeran.pl>
2023-10-21 03:26:21 +0000

    tests: updated unit tests for history module
    FossilOrigin-Name: d27fd40d79f582b42ee2f9515397710b8d194b314497e67665b8dffd09b4e600

--- tests/history.nim
+++ tests/history.nim
@@ -4,40 +4,47 @@ import unittest2
 
 suite "Unit tests for history module":
 
+  checkpoint "Initializing the tests"
   let db = startDb("test8.db".DirectoryPath)
-  assert db != nil, "Failed to initialize the database."
+  require:
+    db != nil
   var commands = newTable[string, CommandData]()
+
+  checkpoint "Initializing the shell's history"
   var amount = initHistory(db, commands)
   if amount == 0:
     discard updateHistory("alias delete", db)
 
-  test "getHistory":
+  test "Getting the shell's history entry":
     check:
       getHistory(1, db) == "alias delete"
 
-  test "historyLength":
+  test "Getting the shell's history length":
     amount = historyLength(db)
     check:
       updateHistory("test comm", db) == amount + 1
 
-  test "showHistory":
+  test "Showing the shell's history":
     check:
      showHistory(db, initLimitedString(capacity = 4, text = "list")) ==
       QuitSuccess
 
-  test "findInHistory":
+  test "Finding text in the shell's history":
+    checkpoint "Finding an exising entry in the history"
     check:
       findInHistory(db, initLimitedString(capacity = 7, text = "find te")) ==
            QuitSuccess
+    checkpoint "Finding a non-exising entry in the history"
+    check:
       findInHistory(db, initLimitedString(capacity = 8,
           text = "find asd")) == QuitFailure
 
-  test "clearHistory":
+  test "Clearing the shell's history":
     check:
       clearHistory(db) == 0
       historyLength(db) == 0
 
-  test "newHistoryEntry":
+  test "Initializing an object of Alias type":
     check:
       newHistoryEntry(command = "newCom").command == "newCom"
 
