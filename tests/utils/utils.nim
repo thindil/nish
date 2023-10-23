c1a7cc5fce44ddd730913c372999997c00a5bd74
thindil <thindil@laeran.pl>
2023-10-23 04:07:04 +0000

    tests: added procedure to add testing aliases
    FossilOrigin-Name: 2106850ffd7aab5ccaf3a930cef57f5f838cd34db325c08d60898bf7c4fec659

--- tests/utils/utils.nim
+++ tests/utils/utils.nim
@@ -1,4 +1,4 @@
-import ../../src/[db, directorypath]
+import ../../src/[aliases, db, directorypath]
 import norm/sqlite
 import unittest2
 
@@ -6,3 +6,12 @@ proc initDb*(dbName: string): DbConn =
   result = startDb(dbName.DirectoryPath)
   require:
     result != nil
+
+proc addAliases*(db: DbConn) =
+  if db.count(Alias) == 0:
+    var alias = newAlias(name = "tests", path = "/", recursive = true,
+          commands = "ls -a", description = "Test alias.", output = "output")
+    db.insert(alias)
+    var testAlias2 = newAlias(name = "tests2", path = "/", recursive = false,
+        commands = "ls -a", description = "Test alias 2.", output = "output")
+    db.insert(testAlias2)
