e72e05c33212448e08b86d3029314c832826f26c
thindil <thindil@laeran.pl>
2023-11-08 05:34:44 +0000

    tests added unit test for readUserInput procedure
    FossilOrigin-Name: a222d8df442b0b48c4b725708983cc71d4dbb676d900f4759861d5003aeb05a1

--- tests/nish.nim
+++ tests/nish.nim
@@ -1,10 +1,27 @@
-import ../src/nish
+import utils/utils
+import std/tables
+import ../src/[commandslist, history, lstring, nish, resultcode]
 import unittest2
 
 suite "Unit tests for nish module":
+
+  checkpoint "Initializing the tests"
+  let db = initDb("test10.db")
+  var
+    myaliases = newOrderedTable[LimitedString, int]()
+    commands = newTable[string, CommandData]()
 
   test "Showing the list of available options for the shell":
     showCommandLineHelp()
 
   test "Showing the shell's version":
     showProgramVersion()
+
+  test "Read the user's input":
+    var
+      iString = initLimitedString(capacity = 100, text = "")
+      cName = "ls"
+      rCode = QuitSuccess.ResultCode
+      hIndex: HistoryRange = 1
+      cPosition: Natural = 1
+    readUserInput(iString, false, db, cName, rCode, hIndex, cPosition, myaliases, commands)
