d3f4af5df71032b42013f95c108711ec1a47f0cc
thindil <thindil@laeran.pl>
2023-11-11 05:38:17 +0000

    tests: added unit tests for suggestion module
    FossilOrigin-Name: 4313ad92dd6845a6ca2fa4d27e64915f362258cf1a290126cefc771a05ff62f6

---
+++ tests/suggestion.nim
@@ -0,0 +1,23 @@
+import std/tables
+import ../src/[commandslist, lstring, suggestion]
+import utils/utils
+import unittest2
+
+suite "Unit tests for aliases module":
+
+  checkpoint "Initializing the tests"
+  let db = initDb("test14.db")
+  var
+    myaliases = newOrderedTable[LimitedString, int]()
+    commands = newTable[string, CommandData]()
+
+  checkpoint "Adding testing aliases if needed"
+  db.addAliases
+
+  test "Fill the suggestions list":
+    fillSuggestionsList(myaliases, commands)
+
+  test "Get suggestion for a command":
+    var start: Natural = 0
+    check:
+      suggestCommand("la", start) == "ln"
