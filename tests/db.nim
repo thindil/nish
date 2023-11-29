8e705e7f2d8c6af23d2214b5ab716d44b6ef6749
thindil <thindil@laeran.pl>
2023-11-29 05:29:30 +0000

    tests: added unit tests for new commands
    FossilOrigin-Name: 97a07b1cac48aa189e1216f753dd6fe39b3132c39726ce80e3fb9f6b077aa90b

--- tests/db.nim
+++ tests/db.nim
@@ -2,6 +2,7 @@ import std/tables
 import ../src/[db, commandslist, lstring, resultcode]
 import utils/utils
 import unittest2
+import norm/sqlite
 
 suite "Unit tests for db module":
 
@@ -17,6 +18,16 @@ suite "Unit tests for db module":
   test "Optimizing the shell's database":
     check:
       optimizeDb(initLimitedString(capacity = 8, text = "optimize"), db) == QuitSuccess
+
+  test "Exporting the shell's database":
+    check:
+      exportDb(initLimitedString(capacity = 15, text = "export test.txt"), db) == QuitSuccess
+
+  test "Importing the shell's database":
+    db.exec("DROP TABLE help".sql)
+    db.exec("DROP TABLE options".sql)
+    check:
+      importDb(initLimitedString(capacity = 15, text = "import test.txt"), db) == QuitSuccess
 
   suiteTeardown:
     closeDb(QuitSuccess.ResultCode, db)
