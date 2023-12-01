979f5f4be3f7ca325e76e1d93e5f48e9af109e96
thindil <thindil@laeran.pl>
2023-12-01 05:31:05 +0000

    feat: started work on the shell's theme
    FossilOrigin-Name: c668dfc80aae110fe722a4a9ff6a54fe94561785b91a96f2ead96201eda61228

---
+++ src/theme.nim
@@ -0,0 +1,80 @@
+# Copyright © 2023 Bartek Jasicki
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## This module contains code related to the shell's colors' theme, like setting
+## them, changing and getting from the database
+
+# Standard library imports
+import std/strutils
+# External modules imports
+import contracts
+import norm/[model, pragmas, sqlite]
+# Internal imports
+
+type
+  ColorName* = enum
+    ## Used to set the colors' value
+    black, red, green, yellow, blue, magenta, cyan, white, default
+  Color* {.tableName: "theme".} = ref object of Model
+    ## Data structure for the shell's color
+    ##
+    ## * name   - the name of the color
+    ## * cValue - the name of the color, in 8 colors pallete
+    command* {.unique.}: string
+    cValue*: ColorName
+
+proc dbType*(T: typedesc[ColorName]): string {.raises: [], tags: [],
+    contractual.} =
+  ## Set the type of field in the database
+  ##
+  ## * T - the type for which the field will be set
+  ##
+  ## Returns the type of the field in the database
+  body:
+    "TEXT"
+
+proc dbValue*(val: ColorName): DbValue {.raises: [], tags: [],
+    contractual.} =
+  ## Convert the type of the colors' value to database field
+  ##
+  ## * val - the value to convert
+  ##
+  ## Returns the converted val parameter
+  body:
+    dbValue(v = $val)
+
+proc to*(dbVal: DbValue, T: typedesc[ColorName]): T {.raises: [], tags: [],
+    contractual.} =
+  ## Convert the value from the database to enumeration
+  ##
+  ## * dbVal - the value to convert
+  ## * T     - the type to which the value will be converted
+  ##
+  ## Returns the converted dbVal parameter
+  body:
+    try:
+      parseEnum[ColorName](s = dbVal.s)
+    except:
+      default
