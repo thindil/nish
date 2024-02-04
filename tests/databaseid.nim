1ff1af35b0dd842a74c22d49d1f8df5788ec2d17
thindil <thindil@laeran.pl>
2024-02-04 04:23:21 +0000

    tests: removed unit test for DatabaseId type
    FossilOrigin-Name: b2257b3ef673a3a98293b78699977eb799d62d5bf388a5a3dc840150d975cc80

--- tests/databaseid.nim
+++
@@ -1,9 +0,0 @@
-import unittest2
-{.warning[UnusedImport]:off.}
-include ../src/databaseid
-
-suite "Unit tests for databaseid module":
-
-  test "Convert DatabaseId to string":
-    check:
-      $12.Databaseid == "12"
