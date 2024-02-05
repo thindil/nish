750c10405bf347be3dc8261ad6642c0fcea2fc95
thindil <thindil@laeran.pl>
2024-02-05 05:11:53 +0000

    tests: updated tests to the new version of getCurrentDirecory procedure
    FossilOrigin-Name: a1539cada6fb1fcfaac18b6f360ca841dbebe5b512dc098ae9aaae0e864b7adb

--- tests/constants.nim
+++ tests/constants.nim
@@ -6,11 +6,11 @@ suite "Unit tests for constant module":
   test "Get the current directory":
     checkpoint "Get an existing current directory"
     check:
-      getCurrentDirectory() == getCurrentDir()
-    let testDir = getCurrentDir() &  DirSep & "test"
+      getCurrentDirectory() == paths.getCurrentDir()
+    let testDir = ospaths2.getCurrentDir() &  DirSep & "test"
     createDir(testDir)
     setCurrentDir(testDir)
     removeDir(testDir)
     checkpoint "Get a non-existing current directory"
     check:
-      getCurrentDirectory() == getHomeDir()
+      getCurrentDirectory().string == getHomeDir()
