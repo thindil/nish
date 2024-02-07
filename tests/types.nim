a79734df089da33f84bcbef361ad9561980ace4c
thindil <thindil@laeran.pl>
2024-02-07 05:09:02 +0000

    tests: added tests for database procedures for Path type
    FossilOrigin-Name: 84705b092ec943ef3ef07e4fa8649db1a6452b1b8ea97882c2e0c055da2d3548

--- tests/types.nim
+++ tests/types.nim
@@ -10,3 +10,16 @@ suite "Unit tests for types module":
   test "Count the length of Path":
     check:
       "/".Path.len == 1
+
+  test "Getting the type of the database field for Path":
+    check:
+      dbType(Path) == "TEXT"
+
+  test "Converting dbValue to Path":
+    check:
+      dbValue("/").s == "/"
+
+  test "Converting Path to dbValue":
+    check:
+      to("/".dbValue, Path) == "/".Path
+
