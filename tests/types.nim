2d87a414287b558f6abc5b40d85891a1cadd1e96
thindil <thindil@laeran.pl>
2024-02-07 04:59:06 +0000

    tests: added unit test for len procedure
    FossilOrigin-Name: e8e0410e3c42957262097e2062c72d7645f65480bf80e80f454a238e2444dd71

--- tests/types.nim
+++ tests/types.nim
@@ -6,3 +6,7 @@ suite "Unit tests for types module":
   test "Convert Path to string":
     check:
       $("/".Path) == "/"
+
+  test "Count the length of Path":
+    check:
+      "/".Path.len == 1
