988e9bd8763c96069b926077a66bc0edfd944c6f
thindil <thindil@laeran.pl>
2024-02-10 05:12:31 +0000

    docs: updated documentation for constants module
    FossilOrigin-Name: 5bfc7256569e3d485a8f04ff74cb2e50cea50cf2e0a8feacf20ab674da0c5e28

--- src/constants.nim
+++ src/constants.nim
@@ -23,7 +23,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-## This module contains constants and variables types used by the shell's code
+## This module contains constants used by the shell's code
 
 # Standard library imports
 import std/[dirs, os, paths]
