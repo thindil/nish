aa60a79b64d833fe60f46a6c3c1f460721637fd0
thindil <thindil@laeran.pl>
2024-05-09 04:33:56 +0000

    tests: fixed some problems reported by nimalyzer
    FossilOrigin-Name: 5547a3be1fb93a6be79d3727d3f747446b3fbd17f4fb4eb27dda34624946563f

--- tests/utils/utils.nim
+++ tests/utils/utils.nim
@@ -1,18 +1,22 @@
 import std/paths
 import ../../src/[aliases, db, types]
 import norm/sqlite
-import unittest2
+import unittest2, contracts
 
-proc initDb*(dbName: string): DbConn =
-  result = startDb(dbName.Path)
-  require:
-    result != nil
+proc initDb*(dbName: string): DbConn {.raises: [], tags: [RootEffect],
+    contractual.} =
+  body:
+    result = startDb(dbPath = dbName.Path)
+    unittest2.require:
+      result != nil
 
-proc addAliases*(db: DbConn) =
-  if db.count(Alias) == 0:
-    var alias = newAlias(name = "tests", path = "/".Path, recursive = true,
+proc addAliases*(db: DbConn) {.raises: [DbError, ValueError], tags: [
+    ReadDbEffect, WriteDbEffect], contractual.} =
+  if db.count(T = Alias) == 0:
+    var alias: Alias = newAlias(name = "tests", path = "/".Path, recursive = true,
           commands = "ls -a", description = "Test alias.", output = "output")
-    db.insert(alias)
-    var testAlias2 = newAlias(name = "tests2", path = "/".Path, recursive = false,
-        commands = "ls -a", description = "Test alias 2.", output = "output")
-    db.insert(testAlias2)
+    db.insert(obj = alias)
+    var testAlias2: Alias = newAlias(name = "tests2", path = "/".Path,
+        recursive = false, commands = "ls -a", description = "Test alias 2.",
+            output = "output")
+    db.insert(obj = testAlias2)
