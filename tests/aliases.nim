9fe1717804e0dc397f9cd12e38198170dc3106ba
thindil <thindil@laeran.pl>
2024-05-11 04:48:10 +0000

    style: added named parameters to calls
    FossilOrigin-Name: 836e8570115ef4e530a6178ec01112c5f2ce55df163196d06c6b9212c45a1010

--- tests/aliases.nim
+++ tests/aliases.nim
@@ -1,13 +1,13 @@
 import utils/utils
 import unittest2
 import ../src/db
-{.warning[UnusedImport]:off.}
+{.warning[UnusedImport]: off.}
 include ../src/aliases
 
 suite "Unit tests for aliases module":
 
   checkpoint "Initializing the tests"
-  let db = initDb("test2.db")
+  let db = initDb(dbName = "test2.db")
   var
     myaliases = newOrderedTable[string, int]()
     commands = newTable[string, CommandData]()
@@ -16,59 +16,57 @@ suite "Unit tests for aliases module":
   db.addAliases
 
   test "Initialization of the shell's aliases":
-    initAliases(db, myaliases, commands)
+    initAliases(db = db, aliases = myaliases, commands = commands)
     check:
       myaliases.len == 1
 
   test "Getting the shell's alias ID":
     checkpoint "Getting ID of an existing alias"
     check:
-      getAliasId("delete 2", db).int == 2
+      getAliasId(arguments = "delete 2", db = db).int == 2
     checkpoint "Getting ID of a non-existing alias"
     check:
-      getAliasId("delete 22", db).int == 0
+      getAliasId(arguments = "delete 22", db = db).int == 0
 
   test "Deleting the shell's alias":
     checkpoint "Deleting an existing alias"
     check:
-      deleteAlias("delete 2",
-        myaliases, db) == QuitSuccess
-      db.count(Alias) == 1
+      deleteAlias(arguments = "delete 2", aliases = myaliases, db = db) == QuitSuccess
+      db.count(T = Alias) == 1
     checkpoint "Deleting a non-existing alias"
     check:
-      deleteAlias("delete 22",
-        myaliases, db) == QuitFailure
-    checkpoint("Re-adding the test alias")
-    var testAlias2 = newAlias(name = "tests2", path = "/".Path, recursive = false,
+      deleteAlias(arguments = "delete 22", aliases = myaliases, db = db) == QuitFailure
+    checkpoint "Re-adding the test alias"
+    var testAlias2 = newAlias(name = "tests2", path = "/".Path,
+      recursive = false,
       commands = "ls -a", description = "Test alias 2.", output = "output")
-    db.insert(testAlias2)
+    db.insert(obj = testAlias2)
     unittest2.require:
-      db.count(Alias) == 2
+      db.count(T = Alias) == 2
 
   test "Setting the shell's aliases in the current directory":
-    myaliases.setAliases(paths.getCurrentDir(), db)
+    myaliases.setAliases(directory = paths.getCurrentDir(), db = db)
     checkpoint "Checking an existing alias"
     check:
-      execAlias("", "tests", myaliases, db) == QuitSuccess
+      execAlias(arguments = "", aliasId = "tests", aliases = myaliases,
+          db = db) == QuitSuccess
     checkpoint "Checking a non existing alias"
     check:
-      execAlias("", "tests2", myaliases, db) == QuitFailure
+      execAlias(arguments = "", aliasId = "tests2", aliases = myaliases,
+          db = db) == QuitFailure
 
   test "Listing the shell's aliases":
     checkpoint "List the shell's aliases in the current directory"
     check:
-      db.count(Alias) == 2
-      listAliases("list", myaliases,
-        db) == QuitSuccess
+      db.count(T = Alias) == 2
+      listAliases(arguments = "list", aliases = myaliases, db = db) == QuitSuccess
     checkpoint "List all available the shell aliases"
     check:
-      listAliases("list all",
-        myaliases, db) == QuitSuccess
+      listAliases(arguments = "list all", aliases = myaliases, db = db) == QuitSuccess
     checkpoint "Check what happen when invalid argument passed to listAliases"
     expect PreConditionDefect:
       check:
-        listAliases("werwerew",
-          myaliases, db) == QuitSuccess
+        listAliases(arguments = "werwerew", aliases = myaliases, db = db) == QuitSuccess
 
   test "Initializing an object of Alias type":
     let newAlias = newAlias(name = "ala")
@@ -76,4 +74,4 @@ suite "Unit tests for aliases module":
       newAlias.name == "ala"
 
   suiteTeardown:
-    closeDb(QuitSuccess.ResultCode, db)
+    closeDb(returnCode = QuitSuccess.ResultCode, db = db)
