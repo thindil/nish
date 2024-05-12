95ca5f3bf9415b5175963befd0ab54a50043a035
thindil <thindil@laeran.pl>
2024-05-12 04:32:39 +0000

    style: added named parameters to calls
    FossilOrigin-Name: 1cb412990d2154164b6baf12c0bb65634ddad68c675c583aa9f436a572d5fda9

--- tests/options.nim
+++ tests/options.nim
@@ -1,62 +1,57 @@
 import utils/utils
 import ../src/[db, history]
 import unittest2
-{.warning[UnusedImport]:off.}
+{.warning[UnusedImport]: off.}
 include ../src/options
 
 suite "Unit tests for options module":
 
   checkpoint "Initializing the tests"
-  let db = initDb("test11.db")
+  let db = initDb(dbName = "test11.db")
   var commands = newTable[string, CommandData]()
-  discard initHistory(db, commands)
+  discard initHistory(db = db, commands = commands)
 
   test "Initializiation of the shell's options":
-    initOptions(commands, db = db)
+    initOptions(commands = commands, db = db)
     check:
       commands.len > 0
 
   test "Getting the value of an option":
     checkpoint "Getting the value of an existing option"
     check:
-      getOption("historyLength",
-          db).len > 0
+      getOption(optionName = "historyLength", db = db).len > 0
     checkpoint "Getting the value of a non-existing option"
     check:
-      getOption("werweewfwe",
-          db).len == 0
+      getOption(optionName = "werweewfwe", db = db).len == 0
 
   test "Adding a new option":
     let optionName = "testOption"
     setOption(optionName = optionName, value = "200", db = db)
     check:
-      deleteOption(optionName, db) == QuitSuccess
-      getOption(optionName, db).len == 0
+      deleteOption(optionName = optionName, db = db) == QuitSuccess
+      getOption(optionName = optionName, db = db).len == 0
 
   test "Updating an existing option":
     setOption(optionName = "historyLength", value = "100", db = db)
     check:
-      getOption("historyLength",
-          db) == "100"
+      getOption(optionName = "historyLength", db = db) == "100"
 
   test "Setting the new value for an option":
     when not defined(testInput):
       skip()
     else:
       check:
-        setOptions(db) == QuitSuccess
-        getOption(initLimitedString(capacity = 13, text = "colorSyntax"),
-            db) == "true"
+        setOptions(db = db) == QuitSuccess
+        getOption(optionName = "colorSyntax", db = db) == "true"
 
   test "Resetting the shell's options":
     check:
-      resetOptions("reset all", db) == QuitSuccess
-      getOption("historyLength",
-          db) == "500"
+      resetOptions(arguments = "reset all", db = db) == QuitSuccess
+      getOption(optionName = "historyLength", db = db) == "500"
 
   test "Showing all options":
     check:
-      showOptions(db) == QuitSuccess
+      showOptions(db = db) == QuitSuccess
 
   test "Initializing an object of Option type":
     check:
@@ -64,15 +59,15 @@ suite "Unit tests for options module":
 
   test "Getting the type of the database field for OptionValType":
     check:
-      dbType(OptionValType) == "TEXT"
+      dbType(T = OptionValType) == "TEXT"
 
   test "Converting dbValue to OptionValType":
     check:
-      dbValue(text).s == "text"
+      dbValue(val = text).s == "text"
 
   test "Converting OptionValType to dbValue":
     check:
-      to(text.dbValue, OptionValType) == text
+      to(dbVal = text.dbValue, T = OptionValType) == text
 
   suiteTeardown:
-    closeDb(QuitSuccess.ResultCode, db)
+    closeDb(returnCode = QuitSuccess.ResultCode, db = db)
