7c6d01fa6d61990e80b5ccfe18bdead9ada33958
thindil <thindil@laeran.pl>
2024-05-17 04:08:17 +0000

    style: added named parameters to calls
    FossilOrigin-Name: 1cf58819db897b3d74a510240e0bb9ba3086a89173c7574dd581d699f939a385

--- tests/db.nim
+++ tests/db.nim
@@ -6,28 +6,28 @@ include ../src/db
 suite "Unit tests for db module":
 
   checkpoint "Initializing the tests"
-  let db = initDb("test15.db")
+  let db = initDb(dbName = "test15.db")
   var commands = newTable[string, CommandData]()
 
   test "Initialization of the shell's database's commands":
-    initDb(db, commands)
+    initDb(db = db, commands = commands)
     check:
       commands.len == 1
 
   test "Optimizing the shell's database":
     check:
-      optimizeDb("optimize", db) == QuitSuccess
+      optimizeDb(arguments = "optimize", db = db) == QuitSuccess
 
   test "Exporting the shell's database":
     check:
-      exportDb("export test.txt", db) == QuitSuccess
+      exportDb(arguments = "export test.txt", db = db) == QuitSuccess
 
   test "Importing the shell's database":
-    db.exec("DROP TABLE help".sql)
-    db.exec("DROP TABLE options".sql)
-    db.exec("DROP TABLE theme".sql)
+    db.exec(query = "DROP TABLE help".sql)
+    db.exec(query = "DROP TABLE options".sql)
+    db.exec(query = "DROP TABLE theme".sql)
     check:
-      importDb("import test.txt", db) == QuitSuccess
+      importDb(arguments = "import test.txt", db = db) == QuitSuccess
 
   suiteTeardown:
-    closeDb(QuitSuccess.ResultCode, db)
+    closeDb(returnCode = QuitSuccess.ResultCode, db = db)
