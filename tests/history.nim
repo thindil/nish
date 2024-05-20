7e12f9bf704e64fab88dab3d71e580050d56cae6
thindil <thindil@laeran.pl>
2024-05-20 04:07:29 +0000

    style: added named parameters to calls
    FossilOrigin-Name: c4ca02755dfedf091d2f2bdbd9db7ce3bcadb25db1d510bc454b183fbaa53ed3

--- tests/history.nim
+++ tests/history.nim
@@ -7,45 +7,45 @@ include ../src/history
 suite "Unit tests for history module":
 
   checkpoint "Initializing the tests"
-  let db = initDb("test8.db")
+  let db = initDb(dbName = "test8.db")
   var commands = newTable[string, CommandData]()
 
   checkpoint "Initializing the shell's history"
-  var amount = initHistory(db, commands)
+  var amount = initHistory(db = db, commands = commands)
   if amount == 0:
-    discard updateHistory("alias delete", db)
+    discard updateHistory(commandToAdd = "alias delete", db = db)
 
   test "Getting the shell's history entry":
     check:
-      getHistory(1, db) == "alias delete"
+      getHistory(historyIndex = 1, db = db) == "alias delete"
 
   test "Getting the shell's history length":
-    amount = historyLength(db)
+    amount = historyLength(db = db)
     check:
-      updateHistory("test comm", db) == amount + 1
+      updateHistory(commandToAdd = "test comm", db = db) == amount + 1
 
   test "Showing the shell's history":
     check:
-     showHistory(db, "list") ==
+     showHistory(db = db, arguments = "list") ==
       QuitSuccess
 
   test "Finding text in the shell's history":
     checkpoint "Finding an exising entry in the history"
     check:
-      findInHistory(db, "find te") ==
+      findInHistory(db = db, arguments = "find te") ==
            QuitSuccess
     checkpoint "Finding a non-exising entry in the history"
     check:
-      findInHistory(db, "find asd") == QuitFailure
+      findInHistory(db = db, arguments = "find asd") == QuitFailure
 
   test "Clearing the shell's history":
     check:
-      clearHistory(db) == 0
-      historyLength(db) == 0
+      clearHistory(db = db) == 0
+      historyLength(db = db) == 0
 
   test "Initializing an object of HistoryEntry type":
     check:
       newHistoryEntry(command = "newCom").command == "newCom"
 
   suiteTeardown:
-    closeDb(QuitSuccess.ResultCode, db)
+    closeDb(returnCode = QuitSuccess.ResultCode, db = db)
