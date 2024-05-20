c8711e5808d1620f9021f13568d83a9245301764
thindil <thindil@laeran.pl>
2024-05-20 04:04:36 +0000

    style: added types information to declarations
    FossilOrigin-Name: 2da3aa6c0d3e125a1d4a2a870543b6312f25068302cce86afabf1f3a24a7397d

--- tests/completion.nim
+++ tests/completion.nim
@@ -34,11 +34,11 @@ include ../src/completion
 suite "Unit tests for completion module":
 
   checkpoint "Initializing the tests"
-  let db = initDb(dbName = "test5.db")
+  let db: DbConn = initDb(dbName = "test5.db")
   var
-    myaliases = newOrderedTable[string, int]()
-    commands = newTable[string, CommandData]()
-    completions: seq[string]
+    myaliases: ref OrderedTable[string, int] = newOrderedTable[string, int]()
+    commands: ref Table[string, CommandData] = newTable[string, CommandData]()
+    completions: seq[string] = @[]
 
   checkpoint "Adding testing aliases if needed"
   db.addAliases
@@ -46,7 +46,7 @@ suite "Unit tests for completion module":
 
   checkpoint "Adding a test completion"
   if db.count(T = Completion) == 0:
-    var completion = newCompletion(command = "ala", cType = custom,
+    var completion: Completion = newCompletion(command = "ala", cType = custom,
         cValues = "something")
     db.insert(obj = completion)
 
@@ -64,7 +64,7 @@ suite "Unit tests for completion module":
       completions[1] == "exit"
 
   test "Initializing an object of Completion type":
-    let newCompletion = newCompletion(command = "ala")
+    let newCompletion: Completion = newCompletion(command = "ala")
     check:
       newCompletion.command == "ala"
 
@@ -94,7 +94,7 @@ suite "Unit tests for completion module":
       deleteCompletion(arguments = "delete 1",
           db = db) == QuitSuccess
       db.count(T = Completion) == 0
-    var completion = newCompletion(command = "ala", cType = custom,
+    var completion: Completion = newCompletion(command = "ala", cType = custom,
         cValues = "something")
     db.insert(obj = completion)
     checkpoint "Deleting a non-existing completion"
